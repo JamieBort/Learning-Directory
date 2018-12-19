58948e5c1ce7008c40d3a26ab509adc96195a76d
Jamie Bort <jamiebort@gmail.com>
2018-12-19 18:09:39 -0500

    Added IntroductionToAJAXWithjQuery\/TheTo-DoListApplication\/ directory.

--- IntroductionToAJAXWithjQuery/YourFirstAjaxApplication/javascript2/pets.js
+++ IntroductionToAJAXWithjQuery/YourFirstAjaxApplication/javascript2/pets.js
@@ -3,18 +3,25 @@ function init() {
     getPetData();
 }
 function getPetData() {
-    var request = new XMLHttpRequest();
-    request.open("GET", "pets.json");
-    request.onreadystatechange = function () {
-        var div = document.getElementById("pets");
+    var request = new XMLHttpRequest(); // Creates an XMLHttpRequest. Assigns it to 'request'.
+    request.open("GET", "pets.json"); // Creates a request from our server for the file 'pest.json';
+    request.onreadystatechange = function () { // Upon response, do the following. In other words, when your ready state changes, call this function.:
+        var div = document.getElementById("pets"); // Get/find the <div> tag with the id of 'pets' in the pest.html file
         if (this.readyState == this.DONE && this.status == 200) {
-            if (this.responseText != null) {
-                div.innerHTML = this.responseText;
+            var type = request.getResponseHeader("Content-Type");
+            console.log("Content-type: " + type);
+            console.log("Status: " + this.statusText);
+
+            // console.log(this.readyState); // My code.
+            // console.log(this.status); // My code.  this.readyState is the ready state of _this object_. In other words, it's the ready state of the request object from line 6 above.
+
+            if (this.responseText != null) { // If the response isn't empty, do the following:
+                div.innerHTML = this.responseText; // insert it into the <div> tag that we got/found on line 9 above.
             }
             else {
-                div.innerHTML = "Error: no data";
+                div.innerHTML = "Error: no data"; // Otherwise insert "Error: no data" into that <div> tag.
             }
         }
     };
-    request.send();
+    request.send(); // Sends the request.
 }
