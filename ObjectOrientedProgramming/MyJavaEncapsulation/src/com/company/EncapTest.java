6f7085c89d7c1098a7d20f4f185b1470bb1646f9
Jamie Bort <jamiebort@gmail.com>
2020-03-01 16:15:57 -0500

    Added ObjectOrientedProgramming directory. Added MyJavaEncapsulation directory.

---
+++ ObjectOrientedProgramming/MyJavaEncapsulation/src/com/company/EncapTest.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class EncapTest {
+    private int myNumber;
+    private String myName;
+
+    public int getMyNumber() {
+        return myNumber;
+    }
+
+    public String getMyName() {
+        return myName;
+    }
+
+    public void setMyNumber(int myNewNumber) {
+//        this.myNumber = myNumber;
+        myNumber = myNewNumber;
+    }
+
+    public void setMyName(String myNewName) {
+//        this.myName = myName;
+        myName = myNewName;
+    }
+}
