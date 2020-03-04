a9500e2d393a47a916c10737e58abec120710c76
Jamie Bort <jamiebort@gmail.com>
2020-03-04 08:04:37 -0500

    Renamed directories.

--- Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section8Arrays_Java_Inbuilt_Lists_Autoboxing_And_Unboxing/104List_And_ArrayListPart1/Arrays,-Java-inbuilt-Lists,-Autoboxing-and-Unboxing-List-and-ArrayList-Part-1-Source-code/src/com/timbuchalka/GroceryList.java
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section8Arrays_Java_Inbuilt_Lists_Autoboxing_And_Unboxing/104List_And_ArrayListPart1/Arrays,-Java-inbuilt-Lists,-Autoboxing-and-Unboxing-List-and-ArrayList-Part-1-Source-code/src/com/timbuchalka/GroceryList.java
@@ -1,9 +1,13 @@
+package com.timbuchalka;
+
 import java.util.ArrayList;
 
 /**
  * Created by dev on 23/08/15.
  */
 public class GroceryList {
+
+
     private ArrayList<String> groceryList = new ArrayList<String>();
 
     public void addGroceryItem(String item) {
@@ -11,6 +15,7 @@ public class GroceryList {
     }
 
     public void printGroceryList() {
+        System.out.println("test");
         System.out.println("You have " + groceryList.size() + " items in your grocery list");
         for(int i=0; i< groceryList.size(); i++) {
             System.out.println((i+1) + ". " + groceryList.get(i));
