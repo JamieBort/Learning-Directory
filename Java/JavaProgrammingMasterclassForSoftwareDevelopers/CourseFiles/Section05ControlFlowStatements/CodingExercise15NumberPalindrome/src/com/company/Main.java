ea798e44a91cec5fe441f175b102d80366f19048
Jamie Bort <jamiebort@gmail.com>
2020-03-10 14:52:35 -0400

    Modified the Main in Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/

--- Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/src/com/company/Main.java
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/src/com/company/Main.java
@@ -5,39 +5,72 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numberArray = new ArrayList<Integer>();
 //        isPalindrome(707);
-        isPalindrome(11221, numberArray);
-
+        isPalindrome(123454321);
     }
 
-//    public static boolean isPalindrome(int number){
-        public static boolean isPalindrome(int number, ArrayList numberArray){
+    public static boolean isPalindrome(int number){
         boolean status = false;
-        createNumberArray(number, numberArray);
-        System.out.println(numberArray);
-
-//        if(createNumberArray(number).get(0)==createNumberArray(number).get(createNumberArray(number).size()-1)){
-        if(numberArray.get(0)==numberArray.get(numberArray.size()-1)){
-            System.out.println("It is true");
-            status=true;
+        ArrayList<Integer> numberArray = createNumberArray(number);
+        System.out.println("The numberArray ArrayList inside of the isPalindrom() method: " + numberArray);
+        System.out.println("The size of the numberArray ArrayList is: " + numberArray.size());
+        int nOI = numberOfIterations(numberArray);
+        System.out.println("the number of iterations: " + nOI);
+        while(nOI>0){
+            compare(numberArray.get(0),numberArray.get(numberArray.size()-1));
+            System.out.println(nOI);
+            nOI -=1;
+//            compare(numberArray.get(0),numberArray.get(numberArray.size()-1));
         }
-        System.out.println(status);
+
+//        if(numberArray.get(0)==numberArray.get(numberArray.size()-1)){
+//            System.out.println("the first and last match.");
+//        }
+//        if(numberArray.get(0+1)==numberArray.get(numberArray.size()-1-1)){
+//            System.out.println("the second and second to last match.");
+//        }
+//        if(numberArray.get(0+1+1)==numberArray.get(numberArray.size()-1-1-1)){
+//            System.out.println("the third and third to last match.");
+//        }
+//
+//
+//        if(numberArray.get(0)==numberArray.get(numberArray.size()-1)){
+//            System.out.println("It is true");
+//            status=true;
+//        }
+        System.out.println("The status is: " + status);
         return status;
     }
 
-    public static ArrayList<Integer> createNumberArray(int number, ArrayList numberArray){
+    public static ArrayList<Integer> createNumberArray(int number){
         System.out.println("the original number: " + number);
-//        ArrayList<Integer> numberArray = new ArrayList<Integer>();
+        ArrayList<Integer> numberArray = new ArrayList<Integer>();
         while (number>0){
-            System.out.println(number%10);
+//            System.out.println(number%10);
             numberArray.add(number%10);
             number=(number-number%10)/10;
-            System.out.println(number);
+//            System.out.println(number);
         }
         System.out.println("The Number Array is: " + numberArray);
         return numberArray;
     }
 
+    public static boolean compare(int a, int b){
+        boolean status = false;
+        if(a==b){
+            System.out.println("inside compare");
+            status = true;
+        }
+        return status;
+    }
+
+    public static int numberOfIterations(ArrayList<Integer> aL){
+        if(aL.size()%2==0){
+            return aL.size()/2;
+        } else {
+            return (aL.size()-1)/2;
+        }
+
+    }
 
 }
