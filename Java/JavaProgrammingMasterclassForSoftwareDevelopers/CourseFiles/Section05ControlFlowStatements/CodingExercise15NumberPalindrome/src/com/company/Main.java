6f4a0b5036a31ef6854edd1afd5fac7f5ebde340
Jamie Bort <jamiebort@gmail.com>
2020-03-09 22:09:53 -0400

    Modified the Main in Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/

--- Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/src/com/company/Main.java
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,6 +11,35 @@ public class Main {
 
     public static boolean isPalindrome(int number){
         boolean status = false;
+
+        System.out.println("the original number: " + number);
+
+        ArrayList<Integer> numberArray = new ArrayList<Integer>();
+        System.out.println(numberArray);
+        numberArray.add(10);
+        numberArray.add(0);
+        numberArray.add(7);
+        System.out.println(numberArray);
+
+
+//        while (number>0){
+//
+//        }
+
+        System.out.println(number%10);
+        number=(number-number%10)/10;
+        System.out.println(number);
+
+//        System.out.println(number);
+        System.out.println(number%10);
+        number=(number-number%10)/10;
+        System.out.println(number);
+
+//        System.out.println(number);
+        System.out.println(number%10);
+        number=(number-number%10)/10;
+        System.out.println("the last: " + number);
+
         if(true){
             status=true;
         }
