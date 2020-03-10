719edb44f9f3a0acd1cd09fe1a5644641262a630
Jamie Bort <jamiebort@gmail.com>
2020-03-10 13:45:36 -0400

    Modified the Main in Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/

--- Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/src/com/company/Main.java
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/src/com/company/Main.java
@@ -5,46 +5,38 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        isPalindrome(707);
+        ArrayList<Integer> numberArray = new ArrayList<Integer>();
+//        isPalindrome(707);
+        isPalindrome(11221, numberArray);
 
     }
 
-    public static boolean isPalindrome(int number){
+//    public static boolean isPalindrome(int number){
+        public static boolean isPalindrome(int number, ArrayList numberArray){
         boolean status = false;
-
-        System.out.println("the original number: " + number);
-
-        ArrayList<Integer> numberArray = new ArrayList<Integer>();
-        System.out.println(numberArray);
-        numberArray.add(10);
-        numberArray.add(0);
-        numberArray.add(7);
+        createNumberArray(number, numberArray);
         System.out.println(numberArray);
 
-
-//        while (number>0){
-//
-//        }
-
-        System.out.println(number%10);
-        number=(number-number%10)/10;
-        System.out.println(number);
-
-//        System.out.println(number);
-        System.out.println(number%10);
-        number=(number-number%10)/10;
-        System.out.println(number);
-
-//        System.out.println(number);
-        System.out.println(number%10);
-        number=(number-number%10)/10;
-        System.out.println("the last: " + number);
-
-        if(true){
+//        if(createNumberArray(number).get(0)==createNumberArray(number).get(createNumberArray(number).size()-1)){
+        if(numberArray.get(0)==numberArray.get(numberArray.size()-1)){
+            System.out.println("It is true");
             status=true;
         }
         System.out.println(status);
         return status;
+    }
+
+    public static ArrayList<Integer> createNumberArray(int number, ArrayList numberArray){
+        System.out.println("the original number: " + number);
+//        ArrayList<Integer> numberArray = new ArrayList<Integer>();
+        while (number>0){
+            System.out.println(number%10);
+            numberArray.add(number%10);
+            number=(number-number%10)/10;
+            System.out.println(number);
+        }
+        System.out.println("The Number Array is: " + numberArray);
+        return numberArray;
     }
 
 
