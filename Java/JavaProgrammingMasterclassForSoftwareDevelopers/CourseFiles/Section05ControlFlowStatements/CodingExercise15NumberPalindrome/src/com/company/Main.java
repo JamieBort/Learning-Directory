ba403446960fb2eece95d489d8145c10183477a9
Jamie Bort <jamiebort@gmail.com>
2020-03-09 21:54:25 -0400

    Added the Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/ directory.

---
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise15NumberPalindrome/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        isPalindrome(707);
+
+    }
+
+    public static boolean isPalindrome(int number){
+        boolean status = false;
+        if(true){
+            status=true;
+        }
+        System.out.println(status);
+        return status;
+    }
+
+
+}
