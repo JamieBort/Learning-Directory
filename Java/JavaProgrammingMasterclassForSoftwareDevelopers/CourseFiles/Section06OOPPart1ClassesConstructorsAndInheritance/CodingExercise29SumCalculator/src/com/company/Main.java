855569213bd360f0e2d63a3732a3d6f6fe8e2880
Jamie Bort <jamiebort@gmail.com>
2020-03-11 15:28:28 -0400

    Finished this exercise.

--- Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section06OOPPart1ClassesConstructorsAndInheritance/CodingExercise29SumCalculator/src/com/company/Main.java
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section06OOPPart1ClassesConstructorsAndInheritance/CodingExercise29SumCalculator/src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add: " + calculator.getAdditionResult());
+        System.out.println("subtract: " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("Multiply: " + calculator.getMultiplictionResult());
+        System.out.println("Divide: " + calculator.getDivisionResult());
     }
 }
