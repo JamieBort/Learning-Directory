3077d8cb153a21ae86c81b039a44a539c1d081b8
Jamie Bort <jamiebort@gmail.com>
2020-03-11 09:27:55 -0400

    Added the Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise16FirstAndLastDigitSum/ directory for the next exercise.

---
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise16FirstAndLastDigitSum/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static int sumFirstAndLastDigit(int number){
+        int sum = 0;
+        if(number<0){
+            sum = -1;
+        } else {
+            
+        }
+        return sum;
+    }
+
+}
