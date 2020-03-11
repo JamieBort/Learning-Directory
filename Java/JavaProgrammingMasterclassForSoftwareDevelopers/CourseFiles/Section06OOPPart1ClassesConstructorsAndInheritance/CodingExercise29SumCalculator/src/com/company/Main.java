57d538f77796b97ec813bffed9a87f84d3251a01
Jamie Bort <jamiebort@gmail.com>
2020-03-11 15:03:39 -0400

    Finished this exercise.

--- Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section06OOPPart1ClassesConstructorsAndInheritance/CodingExercise29SumCalculator/src/com/company/Main.java
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section06OOPPart1ClassesConstructorsAndInheritance/CodingExercise29SumCalculator/src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+        System.out.println("full Name: " + person.getFullName());
+        System.out.println("teen: " + person.isTeen());
+        person.setFirstName("John");
+        person.setAge(18);
+        System.out.println("full Name: " + person.getFullName());
+        System.out.println("teen: " + person.isTeen());
+        person.setLastName("smith");
+        System.out.println("full Name: " + person.getFullName());
     }
 }
