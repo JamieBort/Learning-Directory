d8cf444b05247f7a4f1126f50a733a8426ed63b2
Jamie Bort <jamiebort@gmail.com>
2020-03-11 13:30:32 -0400

    Coppied code to Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/74MinAndMaxChallenge/ from Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/UnknownNumberChallenge/

---
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/74MinAndMaxChallenge/src/com/company/Main.java
@@ -0,0 +1,57 @@
+//-Read the numbers from the console entered by the user and print the minimum and maximum number the user has entered.
+//        -Before the user enters the number, print the message Enter number:
+//        -If the user enters an invalid number, break out of the loop and print the minimum and maximum number.
+//
+//        Hint:
+//        -Use an endless while loop.
+//
+//        Bonus:
+//        -Create a project with the name MinAndMaxInputChallenge.
+
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        collectNumbers();
+    }
+
+    //    method to collect numbers
+    public static ArrayList<Integer> collectNumbers(){
+//        this method is going to collect numbers from the user
+//        add them to an array list
+//        and then return that array list.
+        ArrayList<Integer> anArray = new ArrayList<>();
+        int t=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many integers would you like to enter?");
+        int numberOfIntegers = sc.nextInt();
+        System.out.println("You've chosen " + numberOfIntegers + " integers to enter.");
+        while(t<numberOfIntegers){
+            System.out.println("Please enter an integer.");
+            anArray.add(sc.nextInt());
+            t++;
+        }
+        System.out.println("The ArrayList is: " + anArray);
+        return anArray;
+    }
+
+    //    method to sum numbers
+//    public static int sum(){
+//        ArrayList intArray = collectNumbers();
+//        System.out.println("the length of the arrayList is: " + intArray.size());
+//        int totalSum=0;
+//        if(intArray.size()<1){
+//            totalSum = 0;
+//        } else {
+//            for (int i = 0; i < intArray.size() ; i++) {
+//                totalSum= totalSum + (int) intArray.get(i);
+//            }
+//        }
+//        System.out.println("the sum is: " + totalSum);
+//        return totalSum;
+//    }
+}
