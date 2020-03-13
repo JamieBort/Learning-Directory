c19814e7d8a093eb870ee7bdd7146df215a8afce
Jamie Bort <jamiebort@gmail.com>
2020-03-13 16:17:52 -0400

    Added the Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise27InputCalculator/ directory.

---
+++ Java/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section05ControlFlowStatements/CodingExercise27InputCalculator/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void inputThenPrintSumAndAverage(){
+        sum();
+        average();
+        prompt();
+//        System.out.println("SUM = XX AVG = YY");
+    }
+
+//    method takes array list and sums each entry.
+    public static void sum(){
+        System.out.println("the sum method was called");
+//        int total = 0;
+//        System.out.println("the sum is: " + total);
+    }
+
+//    method prompts user for integer. outputs an ArrayList
+public static void prompt(){
+    System.out.println("the prompt method was called.");
+}
+
+//    method takes the same array list and outputs the average.
+    public static void average(){
+        System.out.println("the average method was called.");
+    }
+
+
+
+}
