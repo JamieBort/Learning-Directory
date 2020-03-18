335082c81b3cceaa66817d8b8a1874907a360067
Jamie Bort <jamiebort@gmail.com>
2020-03-18 16:35:47 -0400

    Followed along with this video.

--- Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section4Modules/28Matplotlib.py
+++ Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section4Modules/28Matplotlib.py
@@ -1,5 +1,16 @@
 # Matplotlib is an external module.
 # External modules need to be installed.
+# to install this matplotlib library type 'pip install matplotlib' in the commnand line.
+# it was already installed. I didn't update it.
+
+# we'll be using these libraries in later lessons:
+# https://matplotlib.org/
+# https://scikit-learn.org/stable/
+# https://www.pygame.org/news
+# https://www.scipy.org/
+# https://www.sqlalchemy.org/
+# https://wxpython.org/
+# https://requests.readthedocs.io/en/master/
 
 import matplotlib.pyplot as plt
 x = [1,2,3,4]
