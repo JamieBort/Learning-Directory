680eaa9364ff928c67eb0177b8db5fbb4ec9750c
Jamie Bort <jamiebort@gmail.com>
2020-03-18 10:42:47 -0400

    Finished this exercise: Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section2DataTypes/12ExerciseListsAndTuples.py

--- Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section2DataTypes/12ExerciseListsAndTuples.py
+++ Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section2DataTypes/12ExerciseListsAndTuples.py
@@ -1,8 +1,11 @@
-mylist = ["one", "two", "three"]
-mytuple = ("one", "two", "three")
-print(mylist[0])
-print(mytuple[0])
-mylist[0]="four"
-# print(mylist[0])
-print(mylist)
-mytuple[0]="four"
+# Started. Not finished.
+
+# mylist = ["one", "two", "three"]
+monthTuble = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+# print("Please enter your birthdate in the formate of MM-DD-YYYY.")
+
+def week(i):
+    switcher={0:'Sunday', 1:'Monday', 2:'Tuesday', 3:'Wednesday', 4:'Thursday', 5:'Friday',6:'Saturday'}
+    return switcher.get(i,"Invalid day of week")
+
+print(week(0))
