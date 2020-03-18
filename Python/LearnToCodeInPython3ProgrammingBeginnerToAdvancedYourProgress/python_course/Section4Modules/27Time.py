30486a614bf8cdc1d0a2d41e6b7e5b13d623b5b5
Jamie Bort <jamiebort@gmail.com>
2020-03-18 15:22:35 -0400

    Finished this video. Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section4Modules/27Time.py

--- Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section4Modules/27Time.py
+++ Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section4Modules/27Time.py
@@ -1,4 +1,13 @@
 import time as t
-time_now= t.localtime()
-print(time_now)
+print("t.localtime(): ", t.localtime())
+time_now = t.localtime()
 print("Transaction completed at: ", str(time_now.tm_hour) + "h:" + str(time_now.tm_min) + "m" )
+time_now = t.time()
+print("Number of seconds since the begining of the epoc: 0 seconds 0 minutes 0 hours January 1, 1970: ", time_now)
+nsiad = 60*60*24 # number of seconds in a day
+delivery_time = time_now + (nsiad + 7)
+print(t.localtime(delivery_time))
+print("Before")
+# print(t.sleep(5))
+# t.sleep(5)
+print("After")
