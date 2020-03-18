9358c258469c25d25c09e2e9e25790fe4cb88a03
Jamie Bort <jamiebort@gmail.com>
2020-03-18 13:56:48 -0400

    Created this Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section3ConditionalsLoopsFunctionsAndABitMore/19ExerciseConditionals.py file. Getting closer.

--- Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section3ConditionalsLoopsFunctionsAndABitMore/19ExerciseConditionals.py
+++ Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section3ConditionalsLoopsFunctionsAndABitMore/19ExerciseConditionals.py
@@ -1,3 +1,7 @@
+def my_function():
+  print("Hello from a function")
+
+
 weight = 0
 height = 1
 # imperial=""
@@ -10,33 +14,24 @@ print("And enter 2 for 'metric'.")
 which_unit = int(input(""))
 print(which_unit)
 
-# if which_unit==1:
-#     print("Imperial was chosen.")
-# elif which_unit==2:
-#     print("Metric was chosen.")
-# else:
-#     print("Neither was chosen.")
-
-# t=0
-# while t<2:
-#     while which_unit!=1 and which_unit!=2:
-#         print("which unit is not 1 or 2")
-#         which_unit=1
-#     print("outer while")
-#     t=t+1
-
 while which_unit!=1 and which_unit!=2:
     print("which unit is not 1 or 2")
-    # which_unit=1
-    which_unit
+    which_unit = int(input(""))
+    print("which unit is not 1 or 2")
+    if which_unit==1:
+        print("Imperial was chosen.")
+    elif which_unit==2:
+        print("Metric was chosen.")
+    else:
+        print("Neither was chosen. Please choose again.")
 
 # while which_unit!=1 and which_unit!=2:
-#     print("which unit is not 1 or 2")
-#     if which_unit==1:
-#         print("Imperial was chosen.")
-#     elif which_unit==2:
-#         print("Metric was chosen.")
-#     else:
-#         print("Neither was chosen. Please choose again.")
+# print("which unit is not 1 or 2")
+if which_unit==1:
+    print("Imperial was chosen.")
+elif which_unit==2:
+    print("Metric was chosen.")
+else:
+    print("Neither was chosen. Please choose again.")
 
 print("we are out of the loop")
