4a60379071153c11f4d2901743fdccd2e1c8eec3
Jamie Bort <jamiebort@gmail.com>
2020-03-19 10:42:28 -0400

    Created this Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section4Modules/29ExerciseTimeAndMatplotlibPyplot.py file. Started this exercise.

---
+++ Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section4Modules/29ExerciseTimeAndMatplotlibPyplot.py
@@ -0,0 +1,26 @@
+# This is from /LearningDirectory/Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section4Modules/27Time.py
+# import time as t
+# print("t.localtime(): ", t.localtime())
+# time_now = t.localtime()
+# print("Transaction completed at: ", str(time_now.tm_hour) + "h:" + str(time_now.tm_min) + "m" )
+# time_now = t.time()
+# print("Number of seconds since the begining of the epoc: 0 seconds 0 minutes 0 hours January 1, 1970: ", time_now)
+# nsiad = 60*60*24 # number of seconds in a day
+# delivery_time = time_now + (nsiad + 7)
+# print(t.localtime(delivery_time))
+# print("Before")
+# # print(t.sleep(5))
+# # t.sleep(5)
+# print("After")
+
+# This is from /LearningDirectory/Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section4Modules/28Matplotlib.py
+# import matplotlib.pyplot as plt
+# x = [1,2,3,4]
+# y = [1500,1200,1100,1800]
+# legend = ["January", "February", "March", "April"]
+# plt.xticks(x,legend)
+# plt.bar(x,y)
+# plt.ylabel("Sales in US$")
+# plt.title("Monthly Sales")
+# plt.plot(x,y)
+# plt.show()
