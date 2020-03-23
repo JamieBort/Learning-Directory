28093640a1a110c7ef019201a80533960c33d685
Jamie Bort <jamiebort@gmail.com>
2020-03-23 00:14:50 -0400

    Java/Go-To-The-Movies/

---
+++ Java/Go-To-The-Movies/src/main/java/com/example/gotothemovies/GoToTheMoviesApplication.java
@@ -0,0 +1,12 @@
+package com.example.gotothemovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoToTheMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoToTheMoviesApplication.class, args);
+	}
+}
---
+++ Java/Go-To-The-Movies/src/main/java/com/example/gotothemovies/entity/MovieResults.java
@@ -0,0 +1,18 @@
+package com.example.gotothemovies.entity;
+
+import java.util.List;
+
+public class MovieResults {
+    private List<Movie> results;
+
+    public MovieResults() {
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+}
