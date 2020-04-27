7f3f492a3b2e9e995ecab45355bdc23a3cd7331c
Jamie Bort <jamiebort@gmail.com>
2020-04-27 15:49:40 -0400

    Modified the Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section3ConditionalsLoopsFunctionsAndABitMore/19ExerciseConditionals.py file.

--- Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section3ConditionalsLoopsFunctionsAndABitMore/19ExerciseConditionals.py
+++ Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section3ConditionalsLoopsFunctionsAndABitMore/19ExerciseConditionals.py
@@ -2,49 +2,18 @@ weight = 0
 height = 1
 bmi_metric = weight/(height * height) # weight in kg. height in meteres
 bmi_imperial = (weight/(height * height)) * 703 # weight in pounds. height in inches
-which_unit=""
+which_unit=0
 
-def unit_question():
+def selecting_a_unit():
+    print("Which unit? Kilograms or pounds?")
+    print("Select 1 for kilograms.")
+    print("Select 2 for pounds.")
     which_unit = int(input(""))
-
-def my_function():
-    print("Hello from a function")
-    print("Which unit do you want to use?")
-    print("Enter 1 for 'imperial'.")
-    print("And enter 2 for 'metric'.")
-    which_unit = int(input(""))
-    # unit_question()
-    print("You've chosen: ", which_unit)
-
-while which_unit!=1 and which_unit!=2:
-    my_function()
-
-
-# print("Which unit do you want to use?")
-# print("Enter 1 for 'imperial'.")
-# print("And enter 2 for 'metric'.")
-# which_unit = int(input(""))
-# print(which_unit)
-
-# while which_unit!=1 and which_unit!=2:
-#     print("which unit is not 1 or 2")
-#     which_unit = int(input(""))
-#     print("which unit is not 1 or 2")
-#     if which_unit==1:
-#         print("Imperial was chosen.")
-#     elif which_unit==2:
-#         print("Metric was chosen.")
-#     else:
-#         print("Neither was chosen. Please choose again.")
-
-# while which_unit!=1 and which_unit!=2: # don't use this.
-# print("which unit is not 1 or 2") # don't use this.
-
-# if which_unit==1:
-#     print("Imperial was chosen.")
-# elif which_unit==2:
-#     print("Metric was chosen.")
-# else:
-#     print("Neither was chosen. Please choose again.")
-
-print("we are out of the loop")
+    if(which_unit == 1):
+        print("Kilograms was selected.")
+    if(which_unit == 2):
+        print("Pounds was selected.")
+    if(which_unit != 1 and which_unit != 2):
+        print("Kilograms nor pounds were selected. Please choose again by selecting either 1. Or 2. But nothing else.")
+selecting_a_unit()
+# print("which unit was selected? ", which_unit)
