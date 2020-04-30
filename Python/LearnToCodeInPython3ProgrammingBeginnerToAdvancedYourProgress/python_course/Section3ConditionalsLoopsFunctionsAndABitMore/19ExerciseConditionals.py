97c695e5f2131dc1395468a0c14404dd3857e64e
Jamie Bort <jamiebort@gmail.com>
2020-04-30 10:03:29 -0400

    Modified the Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section3ConditionalsLoopsFunctionsAndABitMore/19ExerciseConditionals.py file. Done but need to clean it up. Account for error handling.

--- Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section3ConditionalsLoopsFunctionsAndABitMore/19ExerciseConditionals.py
+++ Python/LearnToCodeInPython3ProgrammingBeginnerToAdvancedYourProgress/python_course/Section3ConditionalsLoopsFunctionsAndABitMore/19ExerciseConditionals.py
@@ -2,51 +2,57 @@ weight = 0
 height = 1
 bmi_metric = weight/(height * height) # weight in kg. height in meteres
 bmi_imperial = (weight/(height * height)) * 703 # weight in pounds. height in inches
-variable=0
+unit=0
 
+# function asking the user to choose metric units or imperial units
 def selecting_a_unit():
     which_unit=0
     print("Which unit? Kilograms or pounds?")
     print("Select 1 for kilograms.")
     print("Select 2 for pounds.")
     which_unit = int(input(""))
-    if(which_unit == 1):
-        print("Kilograms was selected.")
-    if(which_unit == 2):
-        print("Pounds was selected.")
     if(which_unit != 1 and which_unit != 2):
         print("Kilograms nor pounds were selected. Please choose again by selecting either 1. Or 2. But nothing else.")
     print("=============================")
     return which_unit
-# selecting_a_unit()
-# print("which unit was selected? ", which_unit)
 
-while variable != 1 and variable != 2:
-    variable = selecting_a_unit()
-    print("which unit is : ", variable)
-    # bmi = 0
-    if variable == 1:
-        print("IT'S KILO")
-        # bmi=calculate_BMI_metric()
-    if variable == 2:
-        print("IT'S POUNDS!!")
-        # bmi = calculate_BMI_imperial()
-    # return bmi
-    # return variable
+def prompting_for_height():
+    print("Please enter your height.")
+    print("It must be greater than 2")
+    height = int(input(""))
+    # height = 1
+    return height
 
-def calculating_BMI(variable, weight, height):
+def prompting_for_weight():
+    print("Please enter your weight.")
+    print("It must be greater than 1")
+    weight = int(input(""))
+    # weight = 1
+    return weight
+
+while unit != 1 and unit != 2:
+    unit = selecting_a_unit()
+    if unit == 1:
+        print(unit, " was selected. IT'S KILO")
+
+    if unit == 2:
+        print(unit, " was selected. IT'S POUNDS!!")
+
+while height < 2:
+    height = prompting_for_height()
+    print("The height is: ", height)
+
+while weight < 1:
+    weight = prompting_for_weight()
+    print("The weight is: ", weight)
+
+def calculating_BMI(unit, weight, height):
     bmi = 0
-    if variable == 1:
+    if unit == 1:
         bmi = weight/(height*height)
-    if variable == 2:
+        print("The bmi is calculated in metric units and is ", bmi,"[put units here]")
+    if unit == 2:
         bmi = 703*weight/(height*height)
-    print("the bmi is: ", bmi)
-calculating_BMI(variable, 1,1)
-# def calculate_BMI_metric():
-#     return weight/(height*height)
+        print("The bmi is calculated in imperial units and is ", bmi,"[put units here]")
 
-# def calculate_BMI_imperial():
-#     return 703*weight/(height*height)
-
-# print("the bmi is: ", bmi)
-print("the variable is :", variable)
+calculating_BMI(unit, weight, height)
