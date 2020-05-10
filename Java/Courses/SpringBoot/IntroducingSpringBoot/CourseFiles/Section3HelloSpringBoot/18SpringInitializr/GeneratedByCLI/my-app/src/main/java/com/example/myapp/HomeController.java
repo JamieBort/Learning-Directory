d18bcaacf53d96265d89bba49dd1688efa30c41d
Jamie Bort <jamiebort@gmail.com>
2020-05-10 17:11:00 -0400

    Created the Java/Courses/SpringBoot/IntroducingSpringBoot/CourseFiles/Section3HelloSpringBoot/18SpringInitializr/GeneratedByCLI/my-app/src/main/java/com/example/myapp/HomeController.java file.

---
+++ Java/Courses/SpringBoot/IntroducingSpringBoot/CourseFiles/Section3HelloSpringBoot/18SpringInitializr/GeneratedByCLI/my-app/src/main/java/com/example/myapp/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello, Spring Boot!";
+    }
+    
+}
