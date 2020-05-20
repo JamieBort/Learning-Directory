e41eb5e89cfe512d4c4a313d5ab94c4bed16a475
Jamie Bort <jamiebort@gmail.com>
2020-05-20 18:29:26 -0400

    Moved the Node/NodePractice/ directory into the Node/Projects/ directory.

---
+++ Node/Projects/NodePractice/express-handlebars/express-handlebars/examples/basic/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express'),
+    exphbs = require('express-handlebars'); // I've replaced line 5 with line 4.
+    // exphbs = require('../../'); // "express-handlebars"
+
+var app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000, function () {
+    console.log('express-handlebars example server listening on: 3000');
+});
