e0b57be302896e9a685ae22aadca9c45a2172047
Jamie Bort <jamiebort@gmail.com>
2020-07-13 05:46:51 -0400

    Figured out string concatenation.

--- JavaScript/CodingChallenges/LeetCode/IntegerToRoman/IntegerToRoman.js
+++ JavaScript/CodingChallenges/LeetCode/IntegerToRoman/IntegerToRoman.js
@@ -13,13 +13,14 @@
 // M             1000
 
 // Examples
-input = 500; // Output: "III"
+input = 3; // Output: "III"
 // input: 4 // Output: "IV"
 // input: 9 // Output: "IX"
 // input: 58 // Output: "LVIII" // Explanation: L = 50, V = 5, III = 3.
 // input: 1994 // Output: "MCMXCIV" // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
   var arrayOfArrays = [["I",1],["V",5],["X",10],["L",50],["C",100],["D",500],["M",1000]];
+  var array = [1,5,10,100,500,1000];
 //  console.log(arrayOfArrays[0][0]);
 //  console.log(arrayOfArrays[0][1]);
  var car = {type:"Fiat", model:"500", color:"white"};
@@ -29,14 +30,18 @@ function parseFunction(input){
     // find how many times 5 goes into 3.
     // It's zero times.
     // if input >= 500, then
-    if(input >= 500){
-        // console.log(input % 500);
-        console.log(10-10%7);
-        return 500 % input;
+    if(input < array[1]){
+        // how many times does 1 go into 3?
+        // console.log(input - input % array[0]);
+        // So we use the array[0] item, input - input % array[0] times.
+        // so call switchFunction(array[0]) input - input % array[0] times.
+        tally = input - input % array[0];
+        array2=[tally,array[0]];
+        return array2;
     }
+    else {console.log(input ,">=", array[0]);}
         // else how many times does 500 go into input?
         // if input < 100....
-    console.log("this function parses the nubmer and calls the switch funcion each time.");
     // switchFunction();
 }
 
@@ -62,17 +67,27 @@ function switchFunction(numeral){
         result = "D";
         break;
         default:
-        result = "I have never heard of that fruit...";
+        result = "None of the options were choses.";
     }
-    console.log(result);
+    // console.log("the result is: ", result);
     return result;
 }
 
- function IntegerToRomans(input){
-     parseFunction(input);
 
-    //  romanNumeral = "III";
-    //  console.log(romanNumeral);
-    //  return romanNumeral;
+function IntegerToRomans(input){
+    // let romanNumberal = "";
+    
+    
+    let parse = parseFunction(input);
+    console.log(parse);
+    a=b="";
+    t=0;
+    while(t < parse[0]){
+        // TODO: concatenate the strings.
+        a = switchFunction(parse[1]);
+        b=b+a;
+        console.log(b);
+        t++;
+    }
  }
  IntegerToRomans(input);
