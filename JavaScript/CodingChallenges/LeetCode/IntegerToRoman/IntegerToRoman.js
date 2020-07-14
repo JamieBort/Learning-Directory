ca8ef46c44add88f8abf5a902435e849a8a4c21c
Jamie Bort <jamiebort@gmail.com>
2020-07-14 03:20:45 -0400

    Working on various cases.

--- JavaScript/CodingChallenges/LeetCode/IntegerToRoman/IntegerToRoman.js
+++ JavaScript/CodingChallenges/LeetCode/IntegerToRoman/IntegerToRoman.js
@@ -17,14 +17,16 @@
 // M             1000
 
 // Examples
+// input = 1; // Output: "I"
+// input = 2; // Output: "II"
 // input = 3; // Output: "III"
-// input = 4; // Output: "IV"
-input = 9; // Output: "IX"
-// input: 58 // Output: "LVIII" // Explanation: L = 50, V = 5, III = 3.
+input = 4; // Output: "IV"
+// input = 9; // Output: "IX"
+// input = 58; // Output: "LVIII" // Explanation: L = 50, V = 5, III = 3.
 // input: 1994 // Output: "MCMXCIV" // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
   var arrayOfArrays = [["I",1],["V",5],["X",10],["L",50],["C",100],["D",500],["M",1000]];
-  var array = [1,5,10,100,500,1000];
+  var array = [1,5,10,50,100,500,1000];
 //  console.log(arrayOfArrays[0][0]);
 //  console.log(arrayOfArrays[0][1]);
  var car = {type:"Fiat", model:"500", color:"white"};
@@ -34,7 +36,28 @@ function parseFunction(input){
     // find how many times 5 goes into 3.
     // It's zero times.
     // if input >= 500, then
+    if(input<array[6]){
+        console.log("input<1000");
+        // return true;
+    }
+    if(input<array[5]){
+        console.log("input< 500");
+        // return true;
+    }
+    if(input<array[4]){
+        console.log("input< 100");
+        // return true;
+    }
+    if(input<array[3]){
+        console.log("input<  50");
+        // return true;
+    }
+    if(input<array[2]){
+        console.log("input<  10");
+        // return true;
+    }
     if(input < array[1]){
+        console.log("input<   5");
         // how many times does 1 go into 3?
         // console.log(input - input % array[0]);
         // So we use the array[0] item, input - input % array[0] times.
@@ -80,9 +103,10 @@ function switchFunction(numeral){
 
 function IntegerToRomans(input){
     let parse = parseFunction(input);
-    console.log(parse);
+    console.log("parse.", parse[2], "appears", parse[1], "times:", parse);
 
     if(parse[0]==false){
+        // what is this controlling for?
         console.log("it was false");
     }else{
         a=b="";
@@ -95,5 +119,6 @@ function IntegerToRomans(input){
             t++;
         }
     }
+    console.log("the ansewr is: ", b)
  }
  IntegerToRomans(input);
