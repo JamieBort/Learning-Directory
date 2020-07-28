7a0a53d24e5eb679da8123f6aefc05eaaa739f64
Jamie Bort <jamiebort@gmail.com>
2020-07-28 10:59:09 -0400

    Mofified files in the JavaScript/CodingChallenges/Pramp/Calculator/ directory.

---
+++ JavaScript/CodingChallenges/Pramp/Calculator/main.js
@@ -0,0 +1,27 @@
+// This file is for figuring out how to cancatinate numbers for the calculator exercise.
+
+// I can use an array and push them into it.
+
+// Or I can use a string and parse that.
+
+// array = [];
+
+// array.push(2);
+// array.push(2);
+
+// console.log(array);
+
+// // want [22]
+// array = [];
+// array = [22];
+// console.log(array);
+
+string = "";
+console.log(string);
+
+string = "2";
+console.log(string);
+string = "2"+ "2";
+string += "3";
+console.log(string);
+console.log(string[3]);
