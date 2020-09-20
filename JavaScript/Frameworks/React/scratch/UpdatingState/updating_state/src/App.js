5f755e3860cec109e8f56378d72b904fd617f6b2
Jamie Bort <jamiebort@gmail.com>
2020-09-20 18:55:47 -0400

    Working on updating state.

--- JavaScript/Frameworks/React/scratch/UpdatingState/updating_state/src/App.js
+++ JavaScript/Frameworks/React/scratch/UpdatingState/updating_state/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
-// var myObj = {
-//   name: 'Umut',
-//   age: 34,
-// };
+var myObj = {
+  name: 'Umut',
+  age: 34,
+  make: 'Ford',
+  model: 'Mustang',
+  year: 1969
+};
 // var myCar = {
 //   make: 'Ford',
 //   model: 'Mustang',
@@ -14,18 +17,16 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      info: "hi",
-      make: 'Ford',
-      model: 'Mustang',
-      year: 1969,
     }
   }
 
   onClickFunction = () => {
-    // console.log(myObj);
     console.log(this.state);
-    this.setState({make: 'Fords'});
-    console.log(this.state);
+    // console.log(this.state);
+    // this.setState({make: 'Fords'});
+    // this.setState({make: myObj});
+    this.setState({myObj});
+    // console.log(this.state);
     // if (this.state.info === "hi") {
     //   this.setState({ info: "bye" });
     // }
@@ -39,6 +40,7 @@ class App extends React.Component {
       <div>
         {/* <span>{this.state.info}</span> */}
         <button onClick={this.onClickFunction}>To update state: {this.state.info}</button>
+        <button onClick={() => console.log(this.state)}>Console log the state.</button>
       </div>
     );
   }
