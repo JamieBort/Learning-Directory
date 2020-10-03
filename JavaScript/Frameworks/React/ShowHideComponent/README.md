7ba69604e6f440f9185ee23cff340af497e089af
Jamie Bort <jamiebort@gmail.com>
2020-10-02 21:13:44 -0400

    First commit. It is working. But not complete.

---
+++ JavaScript/Frameworks/React/ShowHideComponent/README.md
@@ -0,0 +1,3 @@
+# README ShowHideComponent
+following this tutorial to work on showing and hiding components:
+https://www.pluralsight.com/guides/how-to-show-and-hide-reactjs-components
---
+++ JavaScript/Frameworks/React/ShowHideComponent/showhide/src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Demo1 from "./components/Demo1";
+import Demo2 from "./components/Demo2";
+import Demo3 from "./components/Demo3";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      showHideDemo1: false,
+      showHideDemo2: false,
+      showHideDemo3: false
+    };
+    this.hideComponent=this.hideComponent.bind(this);
+  }
+
+  hideComponent(name) {
+    console.log(name);
+    switch (name) {
+      case "showHideDemo1":
+        this.setState({ showHideDemo1: !this.state.showHideDemo1 });
+        break;
+      case "showHideDemo2":
+        this.setState({ showHideDemo2: !this.state.showHideDemo2 });
+        break;
+      case "showHideDemo3":
+        this.setState({ showHideDemo3: !this.state.showHideDemo3 });
+        break;
+      default:
+        // null; // Uncommenting this caused an error.
+    }
+  }
+
+  render() {
+    const { showHideDemo1, showHideDemo2, showHideDemo3 } = this.state;
+    return (
+      <div>
+        {showHideDemo1 && <Demo1 />}
+        <hr />
+        {showHideDemo2 && <Demo2 />}
+        <hr />
+        {showHideDemo3 && <Demo3 />}
+        <hr />
+        <div>
+          <button onClick={() => this.hideComponent("showHideDemo1")}>
+            Click to hide Demo1 component
+          </button>
+          <button onClick={() => this.hideComponent("showHideDemo2")}>
+            Click to hide Demo2 component
+          </button>
+          <button onClick={() => this.hideComponent("showHideDemo3")}>
+            Click to hide Demo3 component
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
+
+export default App;
