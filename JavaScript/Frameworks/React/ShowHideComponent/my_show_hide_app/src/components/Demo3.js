6bb1fb135849747894eca97d23cea6a303687aea
Jamie Bort <jamiebort@gmail.com>
2020-10-09 10:48:06 -0400

    Got the first half of the tutorial working for this JavaScript/Frameworks/React/ShowHideComponent/my_show_hide_app/ directory.

---
+++ JavaScript/Frameworks/React/ShowHideComponent/my_show_hide_app/src/components/Demo3.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Demo3 extends Component{
+    constructor(){ // Why isn't props passed in here?
+        super(); // Why isn't props passed in here?
+        this.state={
+            name: "React"
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div>This is the Demo3 component</div>
+                <hr />
+            </div>
+        )
+    }
+}
+
+export default Demo3;
