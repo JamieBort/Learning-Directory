d392018cd3a36bf6902f583ece2650733670ab81
Jamie Bort <jamiebort@gmail.com>
2020-10-15 08:46:49 -0400

    Added and modified the files in this WebComponents/MyReusableComponent/ directory. Finished creating my first web component.

---
+++ WebComponents/MyReusableComponent/source.js
@@ -0,0 +1,32 @@
+// for the header
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <nav>
+            <button>
+                <a href="./index.html">Home Page button</a>
+            </button>
+            <button>
+                <a href="./blog.html">Blog button</a>
+            </button>
+        </nav>
+        `;
+    }
+}
+
+// for the footer
+class Footer extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <span>Date</span>
+        </footer>
+        `;
+    }
+}
+
+
+
+
+customElements.define('header-web-component', Header);
+customElements.define('footer-web-component', Footer);
