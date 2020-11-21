d60842a164b4104297b9ad1b78ab8ec90f54f1dd
Jamie Bort <jamiebort@gmail.com>
2020-11-21 09:44:18 -0500

    Modified the controler and service again in Java/Courses/SpringBoot/SpringBootQuickStart/

--- Java/Courses/SpringBoot/SpringBootQuickStart/course-api-data/src/main/java/io/javabrains/courseapidata/topic/TopicController.java
+++ Java/Courses/SpringBoot/SpringBootQuickStart/course-api-data/src/main/java/io/javabrains/courseapidata/topic/TopicController.java
@@ -1,9 +1,9 @@
 package io.javabrains.courseapidata.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +16,11 @@ public class TopicController {
     @RequestMapping("/topics") // The default request is GET.
     public List<Topic> getAllTopics(){
         return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}") // The default request is GET.
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
     }
 
 }
--- Java/Courses/SpringBoot/SpringBootQuickStart/course-api-data/src/main/java/io/javabrains/courseapidata/topic/TopicService.java
+++ Java/Courses/SpringBoot/SpringBootQuickStart/course-api-data/src/main/java/io/javabrains/courseapidata/topic/TopicService.java
@@ -15,4 +15,8 @@ public class TopicService {
         return topics;
     }
 
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
 }
