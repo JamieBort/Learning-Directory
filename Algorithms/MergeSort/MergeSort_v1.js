90a8b3b34c779ad4ccc2a4742fb5e78c49598705
Jamie Bort <jamiebort@gmail.com>
2020-12-27 17:57:36 -0500

    The Algorithms/MergeSort/MergeSort_v1.js file/exercise works.

--- Algorithms/MergeSort/MergeSort_v1.js
+++ Algorithms/MergeSort/MergeSort_v1.js
@@ -6,11 +6,10 @@
 
 // Joins them again.
 
-// let givenArray = [3,5,1,-2,17,20];
-let givenArray = [3,5,1,-2];
+let givenArray = [3,5,1,-2,17,20];
+// let givenArray = [3,5,1,-3];
 
 arraySplitter(givenArray);
-
 // 1. fuction to divide the array.
 // Takes in an array as an argument.
 function arraySplitter(array){
@@ -24,6 +23,7 @@ function arraySplitter(array){
         array=sortingFunction(left,right);
         console.log("left", left);
         console.log("right", right);
+        console.log("array", array);
         return array;
     }
 }
@@ -32,19 +32,39 @@ function sortingFunction(left,right){
     let tempArray=[];
     let leftIndex=0;
     let rightIndex=0;
+    if(left[0]>left[1]){
+        a=left[0];
+        left[0]=left[1];
+        left[1]=a;
+    }
+    if(right[0]>right[1]){
+        a=right[0];
+        right[0]=right[1];
+        right[1]=a;
+    }
+
+    // console.log("left", left);
+    // console.log("right", right);
+    // return [-3,1,3,5]
+
+
+    // let tempArray=[];
+    // let leftIndex=0;
+    // let rightIndex=0;
     // if(twoElementArray[0]<twoElementArray[1]){return twoElementArray;}
     while(leftIndex<left.length && rightIndex<right.length){
-        // console.log("while loop");
         if(left[leftIndex]<right[rightIndex]){
-            console.log("left", left);
             tempArray.push(left[leftIndex]);
             leftIndex++;
         } else {
-            console.log("right", right);
             tempArray.push(right[rightIndex]);
             rightIndex++;
         }
-        console.log("tempArray:", tempArray);
+        // console.log("tempArray:", tempArray);
     }
     // console.log("tempArray:", tempArray);
+    // return tempArray;
+    return tempArray
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
 }
