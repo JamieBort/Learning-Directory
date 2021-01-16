1431f13833be8807f2688e4f8d41db417d53fcd9
Jamie Bort <jamiebort@gmail.com>
2021-01-16 14:17:19 -0500

    Added the Bash/BashScripting/ directory and several subdirectories and files.

---
+++ Bash/BashScripting/WritingShellScriptsTheBeginnersGuide/README.md
@@ -0,0 +1,5 @@
+from:
+Writing Shell Scripts — The Beginner’s Guide
+WritingShellScriptsTheBeginnersGuide
+https://medium.com/tech-tajawal/writing-shell-scripts-the-beginners-guide-4778e2c4f609
+
---
+++ Bash/BashScripting/WritingShellScriptsTheBeginnersGuide/my_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "hello world"
+
+#if [ 35 -gt 0 ]
+#then
+#    echo "Greater"
+#fi
+
+#str="Current date is `date`"
+#echo $str
+
+#if [ 35 -gt 45 ]
+#then
+# echo "Greater"
+#else
+# echo "Lesser"
+#fi
+
+for i in 1 2 3
+do
+echo "hello world $i"
+done
