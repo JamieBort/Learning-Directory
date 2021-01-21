5bcf2e4468974673f881fc96bdebb3b3f84fa406
Jamie Bort <jamiebort@gmail.com>
2021-01-21 12:40:37 -0500

    Modified the JavaScript/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/Attempt02.js file. And solved the challenge.

--- JavaScript/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/Attempt02.js
+++ JavaScript/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/Attempt02.js
@@ -2,32 +2,27 @@
 
 // And then return the length of the array.
 
-nums = [1, 1, 2];
+// nums = [1, 1, 2];
+nums = [1, 1, 2, 3];
 // nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
 var removeDuplicates = function (nums) {
     console.log(nums);
     if (nums.length < 2) return nums.length;
 
-    let i = 0;
-    let j = 0;
+    let i = 0; j = 0;
 
-    if (nums[i] == nums[j + 1]) {
-        j++;
-    } else {
-        // console.log("do nothing");
-        j++;
-        i++;
+    for (j = 0; j < nums.length;) {
+        if (nums[i] == nums[j + 1]) {
+            j++; // j=2 i=0
+        } else {
+            nums[i + 1] = nums[j + 1];
+            j++;
+            i++;
+        }
     }
-
-    if (nums[i] == nums[j + 1]) {
-        //     j++;
-    } else {
-        nums[1] = nums[2];
-        j++;
-        i++;
-    }
-    console.log("i=",i,"j=",j,nums);
+    console.log("i=", i, "j=", j, nums);
+    return i;
 }
 
 removeDuplicates(nums);
