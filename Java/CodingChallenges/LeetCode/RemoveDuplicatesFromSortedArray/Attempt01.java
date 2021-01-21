6f228fc38727933a49bb514409b9a045771b49cf
Jamie Bort <jamiebort@gmail.com>
2021-01-21 14:21:33 -0500

    Modified the Java/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/Attempt01.java file.

--- Java/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/Attempt01.java
+++ Java/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/Attempt01.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
+// import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
 public class Attempt01 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] nums = { 1, 1, 2 };
+        int mySecondInteger = myMethod(nums);
+        System.out.print(mySecondInteger);
+        
     }
+
+    static int myMethod(int[] parameterArray) {
+        System.out.print("\n-------------------------\n");
+        System.out.println("Original array: " + Arrays.toString(parameterArray));
+        System.out.print("\n-------------------------\n");
+        System.out.print("Want to return length of 2 and [1, 2, 2]");
+        System.out.print("\n-------------------------\n");
+
+
+        return 2;
+      }
 }
