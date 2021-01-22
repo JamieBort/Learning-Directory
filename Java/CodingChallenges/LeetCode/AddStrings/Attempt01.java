8967cddecf93393296155fee388fbf89c76da2d3
Jamie Bort <jamiebort@gmail.com>
2021-01-22 08:48:32 -0500

    Created the Java/CodingChallenges/LeetCode/AddStrings/Attempt01.java, Java/CodingChallenges/LeetCode/AddStrings/ProblemStatement.md, and Java/CodingChallenges/LeetCode/AddStrings/README.md files.

---
+++ Java/CodingChallenges/LeetCode/AddStrings/Attempt01.java
@@ -0,0 +1,25 @@
+public class Attempt01 {
+    public static void main(String[] args) throws Exception {
+        // System.out.print("Test");
+
+        System.out.print(addStrings("12", "21"));
+        // addStrings("1", "2");
+    }
+
+    // public String addStrings(String num1, String num2) {
+    static String addStrings(String num1, String num2) {
+        // System.out.print(num1+num2);
+        // System.out.print(Integer.parseInt(num1)+Integer.parseInt(num2));
+        str a = num1.charAt(0);
+        // int b = num1.charAt(1);
+        System.out.print(a);
+        System.out.print("\n");
+        // System.out.print(b);
+        // System.out.print("\n");
+        // System.out.print(a+b);
+        // System.out.print("\n");
+        // System.out.print(num1.charAt(0)+num1.charAt(1));
+        return "3";
+    }
+
+}
---
+++ Java/CodingChallenges/LeetCode/AddStrings/README.md
@@ -0,0 +1,18 @@
+# Add Strings
+
+## Status
+Technically solved. However need to solve it again.
+`Attempt01.java` is the latest attempt.
+
+## Next Steps
+`ProblemStatement.md` and `README.md` should be kept.
+`MyVersion`, `old_MyVersion`, and `SomeoneElsesVersion` sould be removed.
+
+
+## Associated Links
+Challange:
+https://leetcode.com/problems/add-strings/
+
+Repo directory:
+https://github.com/JamieBort/LearningDirectory/tree/master/Java/CodingChallenges/LeetCode/AddStrings
+
