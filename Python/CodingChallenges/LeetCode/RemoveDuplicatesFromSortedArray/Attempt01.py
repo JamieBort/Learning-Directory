5cae39e8b8b12e554e21498228cd6a560e3c38c6
Jamie Bort <jamiebort@gmail.com>
2021-01-22 13:15:30 -0500

    Completed this challenge. Python/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/

--- Python/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/Attempt01.py
+++ Python/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/Attempt01.py
@@ -1,9 +1,23 @@
 from typing import List
 
-nums = [1, 2, 3]
+nums = [1, 1, 2]; # want [1, 2, 2]
+# nums = [1, 1, 2, 3]; # want [1, 2, 3, 3]
+# nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
-def removeDuplicates(nums: List[int]) -> int: # omitted "self" from signature.
+def removeDuplicates(nums: List[int]) -> int:# omitted "self" from signature.
 # def removeDuplicates(self, nums: List[int]) -> int:
-  print(nums)
+    if(len(nums)<2):
+        return len(nums)
+    i = 0
+    j = 1
+    while(j < len(nums)):
+        if(nums[i]==nums[j]):
+            j+= 1
+        else:
+            nums[i+1]=nums[j]
+            j+= 1
+            i+= 1
+    return i+1
 
-removeDuplicates(nums)
+print(removeDuplicates(nums))
+# removeDuplicates(nums)
--- Python/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/README.md
+++ Python/CodingChallenges/LeetCode/RemoveDuplicatesFromSortedArray/README.md
@@ -1,5 +1,8 @@
 # Remove Duplicates from Sorted Array
 
+## Status
+Completed this challenge.
+
 [Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/)
 
 https://trello.com/c/NNY3BKH0/711-coding-challenge-with-diana-liubimova
