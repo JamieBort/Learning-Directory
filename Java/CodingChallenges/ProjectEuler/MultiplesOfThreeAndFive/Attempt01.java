a8fc289ba38f196e9ac7a3e096734c02b1b64f84
Jamie Bort <jamiebort@gmail.com>
2021-01-30 16:32:40 -0500

    Solve the problem with the Java/CodingChallenges/ProjectEuler/MultiplesOfThreeAndFive/Attempt01.java file.

--- Java/CodingChallenges/ProjectEuler/MultiplesOfThreeAndFive/Attempt01.java
+++ Java/CodingChallenges/ProjectEuler/MultiplesOfThreeAndFive/Attempt01.java
@@ -3,24 +3,17 @@ import java.util.stream.IntStream;
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 public class Attempt01 {
     public static void main(String[] args) {
-        int[] MultiplesOf3And5Through10;
-        int count = 10;
-        for (int i = 1; i < count + 1; i++) { // want multipls of 3 and 5.
-            if (3%i==0) {
-                System.out.println(i);
+        int total = 0;
+        int count10 = 10;
+        int count1000 = 1000;
+        // int count = count10;
+        int count = count1000;
+        for (int i = 1; i < count; i++) { // want multipls of 3 and 5.
+            if (i%3==0 || i%5==0) {
+                // System.out.println(i);
+                total += i;
             }
         }
-
-        // int[] b = new int[]{3,5,6,9};
-        // int sumb = IntStream.of(b).sum();
-        // System.out.println("The sum is " + sumb);
-
-        // twoSum(new int[]{1,2,3}, 4); // (0, 2)
-
-    }
-
-    public static void twoSum(int[] numbers, int target) {
-        int[] a = new int[] { 0, 2 };
-        System.out.println("a");
+        System.out.println(total);
     }
 }
