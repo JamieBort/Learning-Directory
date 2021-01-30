3656d4263993ec7881913ae8ae2514e4bf0dd707
Jamie Bort <jamiebort@gmail.com>
2021-01-29 21:30:53 -0500

    Created the Java/CodingChallenges/CodeWars/CalculateAverage/Attempt01.java file.

---
+++ Java/CodingChallenges/CodeWars/CalculateAverage/Attempt01.java
@@ -0,0 +1,17 @@
+public class Attempt01 {
+
+    public static void main(String[] args) {
+        // twoSum [1, 2, 3] 4 === (0, 2)
+        
+        twoSum(new int[]{1,2,3}, 4); // (0, 2)
+
+    }
+    
+    public static int[] twoSum(int[] numbers, int target)
+    {
+        int[] a = new int[]{0,2};
+        System.out.println(a);
+        return a;
+        // return null; // Do your magic!
+    }
+}
