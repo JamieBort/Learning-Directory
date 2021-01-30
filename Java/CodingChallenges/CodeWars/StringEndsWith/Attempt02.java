2121362cd5540c446972afa8b1fa34cb18ff98f4
Jamie Bort <jamiebort@gmail.com>
2021-01-29 21:08:28 -0500

    Modified the Java/CodingChallenges/CodeWars/StringEndsWith/Attempt02.java and the Java/CodingChallenges/CodeWars/StringEndsWith/README.md files.

--- Java/CodingChallenges/CodeWars/StringEndsWith/Attempt02.java
+++ Java/CodingChallenges/CodeWars/StringEndsWith/Attempt02.java
@@ -8,16 +8,20 @@
 // This might be helpful too:
 // https://stackoverflow.com/a/513839/8210460
 
-public class Attempt01 {
+public class Attempt02 {
     public static void main(String[] args) throws Exception {
         // my_test("abc", "bc"); // returns true
         // my_test("abc", "d"); // returns false
         // my_test("samurai", "ra"); // returns false
-        my_test("sumo", "omo"); // returns false
-        // my_test("expected", "actual");
+        // my_test("sumo", "omo"); // returns false
+        // my_test("expected", "actual"); // returns false
+        // my_test("ails", "fails"); // returns false
+        // my_test("this", ""); // returns true
+        my_test(":-)", ":-("); // returns false
     }
 
-    // this works except I'm getting an java.lang.StringIndexOutOfBoundsException: error.
+    // this works except I'm getting an java.lang.StringIndexOutOfBoundsException:
+    // error.
     static boolean my_test(String str, String ending) {
         boolean status = false;
 
@@ -29,16 +33,30 @@ public class Attempt01 {
         // System.out.println("the lenght of ending: " + ending_j);
 
         // we need to check letter by letter for the length of "ending".
-        while (ending_j > 0) {
-            if (str.substring(str_i - 1, str_i).equals(ending.substring(ending_j - 1, ending_j))) {
-                // System.out.println("they're equal");
-                status = true;
-            } else {
-                status = false;
+        try {
+            // if (str_i < ending_j) {
+            //     System.out.println("false");
+            //     return false;
+            // }
+            // if (ending=="") {
+            //     System.out.println("true");
+            //     return true;
+            // }
+            while (ending_j > 0) {
+                if (str.substring(str_i - 1, str_i).equals(ending.substring(ending_j - 1, ending_j))) {
+                    // System.out.println("they're equal");
+                    status = true;
+                } else {
+                    // status = false;
+                System.out.println("false");
+                    return false;
+                }
+                // System.out.println(ending_j);
+                ending_j--;
+                str_i--;
             }
-            // System.out.println(ending_j);
-            ending_j--;
-            str_i--;
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Exception occurred . . . . . . . . ");
         }
 
         System.out.println(status);
--- Java/CodingChallenges/CodeWars/StringEndsWith/README.md
+++ Java/CodingChallenges/CodeWars/StringEndsWith/README.md
@@ -4,7 +4,11 @@ from:
 https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/java
 
 ## Status
-20210129
+20210129 1700
+
+Solved. But Attempt02.java needs to be cleaned up a bit. Also, the code could be refined...
+
+20210129 around noon
 
 Just started.
 
