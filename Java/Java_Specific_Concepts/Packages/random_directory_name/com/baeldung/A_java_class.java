37bb4f3198f61506b3c788df485801c82087d2d5
Jamie Bort <jamiebort@gmail.com>
2021-02-05 18:20:43 -0500

    Created the Java/Java_Specific_Concepts/Packages/random_directory_name/com/baeldung/Main.java and the Java/Java_Specific_Concepts/Packages/random_directory_name/com/baeldung/A_java_class.java files.

---
+++ Java/Java_Specific_Concepts/Packages/random_directory_name/com/baeldung/A_java_class.java
@@ -0,0 +1,7 @@
+package com.baeldung;
+
+public class A_java_class {
+    public void a_method(){
+        System.out.println("this does nothing");
+    }
+}
---
+++ Java/Java_Specific_Concepts/Packages/random_directory_name/com/baeldung/Main.java
@@ -0,0 +1,11 @@
+package com.baeldung;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Next to study is Java Bean");
+        A_java_class a_java_class = new A_java_class();
+        a_java_class.a_method();
+        // a_java_class a_java_class = new a_java_class()
+
+    }
+}
