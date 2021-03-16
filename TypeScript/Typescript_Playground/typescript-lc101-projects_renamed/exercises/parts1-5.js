a6b7bc919fb72f055dbdccd73e0e7d552e90c149
Jamie Bort <jamiebort@gmail.com>
2021-03-16 17:27:28 -0400

    Modified the TypeScript/Typescript_Playground/typescript-lc101-projects_renamed/exercises/parts1-5.ts file and added the TypeScript/Typescript_Playground/typescript-lc101-projects_renamed/exercises/parts1-5.js file.

--- TypeScript/Typescript_Playground/typescript-lc101-projects_renamed/exercises/parts1-5.js
+++ TypeScript/Typescript_Playground/typescript-lc101-projects_renamed/exercises/parts1-5.js
@@ -1,65 +1,31 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
-
-
 // Part 1: Declare (5) Variables With Type
-
-let spacecraftName:	string = 'Determination';
-let speedMph: number = 17500;
-let kilometersToMars:	number	= 225000000;
-let kilometersToTheMoon:	number	= 384400;
-let milesPerKilometer: number =	0.621;
-
-
-
+var spacecraftName = 'Determination';
+var speedMph = 17500;
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
+var milesPerKilometer = 0.621;
 // Part 2: Print Days to Mars
 // In the same file you opened in Part 1, do the following.
-
 // Declare and assign these variables.
 // Remember: variable declarations in TypeScript include the type!
-let milesToMars: number = kilometersToMars * milesPerKilometer;
-let hoursToMars : number = milesToMars / speedMph;
-let daysToMars: number = hoursToMars / 24;
-
+var milesToMars = kilometersToMars * milesPerKilometer;
+var hoursToMars = milesToMars / speedMph;
+var daysToMars = hoursToMars / 24;
 // Write a console.log statement that prints out the days to Mars.
 // Use template literal syntax and the variables ${spacecraftName} and ${daysToMars}.
-console.log(`The spacecraft name is ${spacecraftName} and the days to Mars is ${daysToMars}.`)
-
+console.log("The spacecraft name is " + spacecraftName + " and the days to Mars is " + daysToMars + ".");
 // Use the terminal in VSCode to compile your .ts file, then use the command node parts1-5.js to run the JavaScript file created during the build process.
-
-
 // Code an output statement here (use a template literal):
-
-
-
 // Part 3: Create a Function ("getDaysToLocation")
-
-
-
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
-
-
-
-
 // Part 4: Create a Spacecraft Class
-
-
-
-
 // Create an instance of the class here:
-
-
-
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-
-
-
 // Part 5: Export and Import the SpaceLocation Class
 // Add the required import statement BEFORE the part 1 concent.
-
-
 // Add the printDaysToLocation function to the Spacecraft class.
-
 // Paste in the code from step 6 here:
