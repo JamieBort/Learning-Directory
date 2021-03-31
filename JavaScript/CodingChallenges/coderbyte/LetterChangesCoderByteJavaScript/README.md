3225853fd9d0b631a808ae87875c5181676253cb
Jamie Bort <jamiebort@gmail.com>
2021-03-31 12:49:12 -0400

    Finished with the JavaScript/CodingChallenges/coderbyte/LetterChangesCoderByteJavaScript/ files.

--- JavaScript/CodingChallenges/coderbyte/LetterChangesCoderByteJavaScript/README.md
+++ JavaScript/CodingChallenges/coderbyte/LetterChangesCoderByteJavaScript/README.md
@@ -5,10 +5,7 @@ https://www.coderbyte.com/editor/guest:Letter%20Changes:JavaScript
 
 ## Status
 This one is now behind a paywall.
-~~And I think AlphabetSoup.js already solved this one.~~
-~~But I will attempt to solve it anyay.~~
-~~Solved - I think. See attempt01.js ~~
-~~I cannot verify with their site.~~
+**Lost interest. Not enough of a challenge.**
 
 ## Challenge
 Using the JavaScript language, have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
--- JavaScript/CodingChallenges/coderbyte/LetterChangesCoderByteJavaScript/attempt01.js
+++ JavaScript/CodingChallenges/coderbyte/LetterChangesCoderByteJavaScript/attempt01.js
@@ -2,37 +2,31 @@
 // Using the JavaScript language, have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string. 
 
 // Sample Test Cases
-// Input:"hello*3"
+// const str = "hello*3"; // Input
 // Output:"Ifmmp*3"
 
-// Input:"fun times!"
+// const str ="fun times!" // Input
 // Output:"gvO Ujnft!"
 
-var str = "hello*3"
+const str = "zoo";
+// A-Z is 65-90, a-z is 97-122
 function LetterChange(string) {
     let tempString = "";
-    console.log("letter at place 0: ", str.charAt(0));
-    console.log("code for first element:", str.charCodeAt(0));
-    tempString += String.fromCharCode(str.charCodeAt(0)+1);
-    console.log("tempString: ", tempString);
+    for (let letter of str) {
+        if ((letter.charCodeAt() > 64 && letter.charCodeAt() < 91) || (letter.charCodeAt() > 96 && letter.charCodeAt() < 123)) { // checks if the character is a-z or A-Z
+            if (letter.charCodeAt() == 122) { tempString += String.fromCharCode(letter.charCodeAt() - 25) }
+            else if (letter.charCodeAt() == 100 || letter.charCodeAt() == 104 || letter.charCodeAt() == 110 || letter.charCodeAt() == 116) { // checks if the character is a vowel.
+                let temp = String.fromCharCode(letter.charCodeAt() - 31); // if so, reasign it by subtracting 31 from it.
+                let temp2 =temp.toUpperCase();
+                console.lot(temp2);
+                tempString += temp2;
 
-
-
-
-
-
-
-    // console.log("tempString: ", tempString);
-    // tempString+="a";
-    // tempString += String.fromCharCode(189, 43, 190, 61);
-    // console.log("tempString: ", tempString);
-
-    // console.log("letter at place 1: ",str.charAt(0));// returns letter at the 1 place.
-    // const abc = String.fromCharCode(189, 43, 190, 61);
-    // console.log("abc: ", abc);
-
-    // str.at(3);
-    // console.log("code for first element:",str.charCodeAt(0));
-
+// console.log("tempString");
+            }
+            else { tempString += String.fromCharCode(letter.charCodeAt() + 1); } // if not, reassign it by adding 1 to it.
+        }
+        else { tempString += letter; } // if not, reappend it to the string.
+    }
+    console.log(tempString);
 }
 LetterChange(str);
