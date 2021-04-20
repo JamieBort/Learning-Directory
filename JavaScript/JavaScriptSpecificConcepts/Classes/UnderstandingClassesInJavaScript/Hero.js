cc3b4fe65dc8bbf3aedbd8b78dde9cd0ca6b3e6a
Jamie Bort <jamiebort@gmail.com>
2021-04-19 20:35:36 -0400

    Modified two files in the JavaScript/JavaScriptSpecificConcepts/Classes/UnderstandingClassesInJavaScript/ directory.

--- JavaScript/JavaScriptSpecificConcepts/Classes/UnderstandingClassesInJavaScript/Hero.js
+++ JavaScript/JavaScriptSpecificConcepts/Classes/UnderstandingClassesInJavaScript/Hero.js
@@ -7,29 +7,7 @@ export default class Hero {
 
     // Adding a method to the constructor
     greet() {
-
         console.log(`${this.name} says hello.`);
         return `${this.name} says hello.`;
     }
 }
-
-// // Creating a new class from the parent
-// class Mage extends Hero {
-//     constructor(name, level, spell) {
-//         // Chain constructor with super
-//         super(name, level);
-
-//         // Add a new property
-//         this.spell = spell;
-//     }
-// }
-
-// const hero1 = new Hero('Varg', 1);
-// console.log(hero1);
-
-// const hero2 = new Mage('Lejon', 2, 'Magic Missile');
-// console.log(hero2);
-
-// // let hero = new Hero("Jim", 3);
-// // hero.greet();
-
--- JavaScript/JavaScriptSpecificConcepts/Classes/UnderstandingClassesInJavaScript/Mage.js
+++ JavaScript/JavaScriptSpecificConcepts/Classes/UnderstandingClassesInJavaScript/Mage.js
@@ -1,24 +1,4 @@
 import Hero from './Hero';
-// // Initializing a class definition
-// class Hero {
-//     constructor(name, level) {
-//         this.name = name;
-//         this.level = level;
-//     }
-
-//     // Adding a method to the constructor
-//     greet() {
-
-//         console.log(`${this.name} says hello.`);
-//         return `${this.name} says hello.`;
-//     }
-// }
-
-// const hero1 = new Hero('Varg', 1);
-// console.log(hero1);
-
-// // let hero = new Hero("Jim", 3);
-// // hero.greet();
 
 // Creating a new class from the parent
 export default class Mage extends Hero {
