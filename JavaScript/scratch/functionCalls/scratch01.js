116ab41246939075eda3636c7a54f0240efd50fc
Jamie Bort <jamiebort@gmail.com>
2021-04-20 17:00:08 -0400

    Added the JavaScript/scratch/functionCalls directory.

---
+++ JavaScript/scratch/functionCalls/scratch01.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+            textDisplay: false,
+            texttext : "On",
+        }
+      // this.text = "On";
+  }
+
+  handleClick() {
+    // todo
+    console.log("it was pushed");
+    this.setState((currentState) => ({
+            textDisplay: !currentState.textDisplay, 
+            texttext:"OFF",
+        }));
+  }
+
+  render() {
+    console.log("This runs automatically.");
+    return (
+      <button onClick={()=>this.handleClick()}>{this.state.texttext}</button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
+
+// using: https://medium.com/path2code/how-react-js-toggle-button-works-99c838ae2fe1
