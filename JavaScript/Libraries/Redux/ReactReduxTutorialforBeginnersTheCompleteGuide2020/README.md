132cde8ee6bf1aa192a0b1328ae01add1774ed35
Jamie Bort <jamiebort@gmail.com>
2021-04-21 17:22:55 -0400

    Modified the JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/README.md and JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/react-redux-tutorial/src/js/reducers/index.js files.

--- JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/README.md
+++ JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/README.md
@@ -4,16 +4,26 @@ Following this tutorial to learn Redux better.
 
 ## Status
 I read through up to this section:
-https://www.valentinog.com/blog/redux/#react-redux-tutorial-getting-to-know-redux-actions-and-named-constants
+https://www.valentinog.com/blog/redux/#react-redux-tutorial-redux-store-methods
 
 ## Notes
+Ran `npm i redux --save-dev` in the `react-redux-tutorial` root directory. Which reslted in this Redux dependency
+```
+  "devDependencies": {
+    "redux": "^4.0.5"
+  }
+```
+ in the `package.json` file.
+
 Store - holds all of the application's state.
-see `src/js/store/index.js`
+See `src/js/store/index.js`
 
 Reducers - Redux reducer is just a JavaScript function. It takes two parameters: the current state and action. Returns the (new) state.
 See `src/js/reducers/index.js`
 
 Actions - JavaScript object consisting of type and payload. The type property drives how the state should change and it's always required by Redux. The payload property instead describes what should change, and might be omitted.
+
+Change state by dispatching an action to the store.
 
 Action Creator - best practice in Redux we wrap every action within a function, so that object creation is abstracted away. Such function takes the name of action creator: let’s put everything together by creating a simple action creator.
 See `src/js/actions/index.js`
--- JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/react-redux-tutorial/src/js/reducers/index.js
+++ JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/react-redux-tutorial/src/js/reducers/index.js
@@ -2,12 +2,48 @@
 
 // Reducers - Redux reducer is just a JavaScript function. It takes two parameters: the current state and action. Returns the (new) state.
 
+// const initialState = {
+//     articles: []
+//   };
+  
+//   function rootReducer(state = initialState, action) { // **** This reducer does nothing. So we refactor it below. ***
+//     return state;
+//   };
+  
+//   export default rootReducer;
+
+// ************************************************************
+
+// import { ADD_ARTICLE } from "../constants/action-types";
+
+// const initialState = {
+//   articles: []
+// };
+
+// function rootReducer(state = initialState, action) {
+//   if (action.type === ADD_ARTICLE) {
+//     state.articles.push(action.payload); // *** Array.prototype.push is an impure function: it modifies the original array. But there's more. We're also changing the initial state in place. So we re-factory once more. See below. ***
+//   }
+//   return state;
+// }
+
+// export default rootReducer;
+
+// ************************************************************
+
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
-    articles: []
-  };
-  
-  function rootReducer(state = initialState, action) {
-    return state;
-  };
-  
-  export default rootReducer;
+  articles: []
+};
+
+function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, { // use Object.assign or object spread of objects to avoid mutations in Redux.
+      articles: state.articles.concat(action.payload) // use concat, slice, or the spread operator for arrays to avoid mutations in Redux.
+    });
+  }
+  return state;
+}
+
+export default rootReducer;
