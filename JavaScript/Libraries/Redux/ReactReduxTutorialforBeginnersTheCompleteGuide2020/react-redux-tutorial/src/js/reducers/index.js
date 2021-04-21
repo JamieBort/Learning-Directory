b78cf51b00234869aa2aca6ba08ab3bc48dcfb84
Jamie Bort <jamiebort@gmail.com>
2021-04-21 13:40:57 -0400

    Created the JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/react-redux-tutorial/src/js/reducers/ directory and the JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/react-redux-tutorial/src/js/reducers/index.js file.

---
+++ JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/react-redux-tutorial/src/js/reducers/index.js
@@ -0,0 +1,11 @@
+// src/js/reducers/index.js
+
+const initialState = {
+    articles: []
+  };
+  
+  function rootReducer(state = initialState, action) {
+    return state;
+  };
+  
+  export default rootReducer;
