9524923b770e0d08cbb23a7f00702063dc0c6bd7
Jamie Bort <jamiebort@gmail.com>
2021-04-22 08:51:10 -0400

    Created the JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/02/react-redux-tutorial/src/js/components/ directory and the JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/02/react-redux-tutorial/src/js/components/App.js file.

---
+++ JavaScript/Libraries/Redux/ReactReduxTutorialforBeginnersTheCompleteGuide2020/02/react-redux-tutorial/src/js/components/App.js
@@ -0,0 +1,12 @@
+// src/js/components/App.js
+import React from "react";
+import List from "./List";
+
+const App = () => (
+  <div>
+    <h2>Articles</h2>
+      <List />
+  </div>
+);
+
+export default App;
