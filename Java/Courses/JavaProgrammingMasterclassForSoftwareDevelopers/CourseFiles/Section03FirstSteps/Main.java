fbf6c6bbb6aab04b894a6e763dc67b22669ce47b
Jamie Bort <jamiebort@gmail.com>
2021-05-03 20:12:47 -0400

    Modified the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section03FirstSteps/Main.java file.

--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section03FirstSteps/Main.java
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section03FirstSteps/Main.java
@@ -1,12 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        // getDurationString(60,4);
-        System.out.println("Hello");
+        // byte_long_sort_width_26 abc = new byte_long_sort_width_26();
+        // abc.getDurationString(60, 4);
+
+        Primitive_Types_Challenge_28 cba = new Primitive_Types_Challenge_28();
+        cba.primative_types_challenge();
+        // System.out.println("Hello");
     }
-
-    // public static String getDurationString(int minutes, int seconds){
-        
-
-    // }
 }
