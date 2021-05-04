e49654459e36748fb0266a522d7a3584968b2406
Jamie Bort <jamiebort@gmail.com>
2021-05-03 20:10:15 -0400

    Created the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore/Main.java file.

---
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // 59SecondsAndMinutesChallenge
+        SecondsAndMinutesChallenge_59.getDurationString(60,4);
+        
+        // System.out.println("Hello");
+    }
+}
