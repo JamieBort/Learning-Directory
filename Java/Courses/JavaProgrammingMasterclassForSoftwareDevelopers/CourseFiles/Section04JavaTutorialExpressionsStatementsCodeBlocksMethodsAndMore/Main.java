64bba25b706010aa66874cb1cbaad54be494a433
Jamie Bort <jamiebort@gmail.com>
2021-05-04 11:02:00 -0400

    Modified the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore/Main.java file.

--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore/Main.java
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore/Main.java
@@ -1,9 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        // 59SecondsAndMinutesChallenge
-        SecondsAndMinutesChallenge_59.getDurationString(60,4);
-        
+        Section59SecondsAndMinutesChallenge.getDurationString(60, 4);
         // System.out.println("Hello");
     }
 }
