46913887cc2e0df288d4b09059efd7a7c7616b1d
Jamie Bort <jamiebort@gmail.com>
2021-05-04 11:03:55 -0400

    Created the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section08Arrays_Java_Inbuilt_Lists_Autoboxing_And_Unboxing/100ArraysChallengeExercise/ directory.

---
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section08Arrays_Java_Inbuilt_Lists_Autoboxing_And_Unboxing/100ArraysChallengeExercise/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Section59SecondsAndMinutesChallenge.getDurationString(60, 4);
+        System.out.println("Hello");
+    }
+}
