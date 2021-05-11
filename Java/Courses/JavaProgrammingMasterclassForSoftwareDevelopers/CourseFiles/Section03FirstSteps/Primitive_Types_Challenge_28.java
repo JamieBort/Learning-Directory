71642bf60e9cdbfa3914afeb5e62c9f26d69ef72
Jamie Bort <jamiebort@gmail.com>
2021-05-11 11:49:44 -0400

    Modified these two java files.

--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section03FirstSteps/Primitive_Types_Challenge_28.java
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section03FirstSteps/Primitive_Types_Challenge_28.java
@@ -1,11 +1,13 @@
+package Section03FirstSteps;
+
 public class Primitive_Types_Challenge_28 {
 
-    static void primative_types_challenge(byte byteParameter,short shortParameter, int intParameter) {
+    public void primative_types_challenge(byte byteParameter,short shortParameter, int intParameter) {
         long result = (10L * (byteParameter + shortParameter + intParameter));
         System.out.println("result with three parameters: " + result);
     }
 
-    static void primative_types_challenge(byte byteParameter,short shortParameter, int intParameter, long longParameter) {
+    public void primative_types_challenge(byte byteParameter,short shortParameter, int intParameter, long longParameter) {
         long result = longParameter + (10L * (byteParameter + shortParameter + intParameter));
         System.out.println("longParameter: " + longParameter);
         System.out.println("byteParameter: " + byteParameter);
@@ -14,5 +16,15 @@ public class Primitive_Types_Challenge_28 {
         // long longParameter = 50000L + (10L * (byteParameter + shortParameter + intParameter));
         System.out.println("result with four parameters: " + result);
     }
+
+    // public void primative_types_challenge(byte byteParameter,short shortParameter, int intParameter, Long longParameter) {
+    //     long result = longParameter + (10L * (byteParameter + shortParameter + intParameter));
+    //     System.out.println("longParameter: " + longParameter);
+    //     System.out.println("byteParameter: " + byteParameter);
+    //     System.out.println("shortParameter: " + shortParameter);
+    //     System.out.println("intParameter: " + intParameter);
+    //     // long longParameter = 50000L + (10L * (byteParameter + shortParameter + intParameter));
+    //     System.out.println("result with four parameters: " + result);
+    // }
 
 }
--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section03FirstSteps/byte_long_sort_width_26.java
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section03FirstSteps/byte_long_sort_width_26.java
@@ -1,3 +1,5 @@
+package Section03FirstSteps;
+
 public class byte_long_sort_width_26 {
 
     public static String getDurationString(int minutes, int seconds){
