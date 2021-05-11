ac5b92fcf3e82f43183eea61688b9b9badf5ec39
Jamie Bort <jamiebort@gmail.com>
2021-05-11 11:44:52 -0400

    Created the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Main.java file.

---
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Main.java
@@ -0,0 +1,15 @@
+// import Section03FirstSteps.Section03Methods;
+import Section03FirstSteps.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Section03Methods section03methods = new Section03Methods();
+        section03methods.Floating_Point_Precision_and_a_Challenge_30();
+        // section03methods.float_and_double_Primitive_Types_29();
+
+        // Section04Methods() section04methods = new Section04Methods();
+
+        // Section05Methods() section05methods = new Section05Methods();
+    }
+}
