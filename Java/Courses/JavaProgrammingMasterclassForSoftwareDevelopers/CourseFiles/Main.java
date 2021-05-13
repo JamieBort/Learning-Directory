3e3426be7617548f465fb10022df7f8bfad13a6e
Jamie Bort <jamiebort@gmail.com>
2021-05-13 13:15:46 -0400

    Modified two markdown files in the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/ directory.

--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Main.java
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Main.java
@@ -2,6 +2,7 @@ import Section03FirstSteps.*;
 import Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore.*;
 import Section05ControlFlowStatements.*;
 import Section06OOPPart1ClassesConstructorsAndInheritance.*;
+import Section09InnerAndAbstractClassesAndInterfaces.*;
 
 public class Main {
 
@@ -14,17 +15,21 @@ public class Main {
         // // section04methods.Seconds_and_Minutes_Challenge_60(2,4);
         // section04methods.Seconds_and_Minutes_Challenge_60(61,59);
 
-        Section05Methods section05methods = new Section05Methods();
-        // section05methods.The_Switch_Statement_Challenge_Exercise_63();
-        // section05methods.Coding_Exercise_16_First_And_Last_Digit_Sum();
-        // section05methods.Coding_Exercise_15_Number_Palindrome();
-        section05methods.Coding_Exercise_27_Input_Calculator();
+        // Section05Methods section05methods = new Section05Methods();
+        // // section05methods.The_Switch_Statement_Challenge_Exercise_63();
+        // // section05methods.Coding_Exercise_16_First_And_Last_Digit_Sum();
+        // // section05methods.Coding_Exercise_15_Number_Palindrome();
+        // section05methods.Coding_Exercise_27_Input_Calculator();
 
         // Section06Methods section06methods = new Section06Methods();
         // section06methods.Coding_Exercise_29_Sum_Calculator();
         // section06methods.Coding_Exercise_30_Person();
         // section06methods.Constructors_Part_1_Challenge_Exercise_79();
         // section06methods.Constructors_Part_1_Challenge_Exercise_80();
+
+        Section09Methods section09methods = new Section09Methods();
+        // section09methods.The_Switch_Statement_Challenge_Exercise_63();
+        section09methods.printing();
         
     }
 }
--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/WhatIveLearned/Section09InnerAndAbstractClassesAndInterfaces.md
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/WhatIveLearned/Section09InnerAndAbstractClassesAndInterfaces.md
@@ -4,3 +4,13 @@
 
 ## Section 9 - Inner and Abstract Classes & Interfaces
 Section09InnerAndAbstractClassesAndInterfaces
+
+### 123 - Interfaces
+* Interfaces specifiy methods that a particular class that implements the interface must implement.
+* In other words, if a class implements an interface, then the methods in that interface must be implemented by that class.
+* it's abstract - no code (logic?) is writen. only the methods
+* forces common behavior among all the classes that implement the interface.
+* on other words standardize way a set of classes is used
+* in the body of the interface function signatures are writen, but not the logic - that is left up to each class the implements the interface.
+* access modifiers are not needed in the function signatures of interfaces.
+* **use `implements` keyword**, not `extends` keyword.
