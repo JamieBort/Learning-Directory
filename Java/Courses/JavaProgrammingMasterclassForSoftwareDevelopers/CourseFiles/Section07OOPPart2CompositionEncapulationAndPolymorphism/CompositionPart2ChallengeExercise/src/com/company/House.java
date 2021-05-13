8b7af5a9ba79823f2be168b855100afad6fe78d4
Jamie Bort <jamiebort@gmail.com>
2021-05-13 10:01:55 -0400

    Deleted the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section07OOPPart2CompositionEncapulationAndPolymorphism/CompositionPart2ChallengeExercise/ directory.

--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section07OOPPart2CompositionEncapulationAndPolymorphism/CompositionPart2ChallengeExercise/src/com/company/House.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class House {
-    private LivingRoom livingRoom; //    a field. This is a has-a relationship.
-//    constructor
-//    methods
-}
--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section07OOPPart2CompositionEncapulationAndPolymorphism/CompositionPart2ChallengeExercise/src/com/company/LivingRoom.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class LivingRoom {
-//    fields
-//    constructor
-//    methods
-}
