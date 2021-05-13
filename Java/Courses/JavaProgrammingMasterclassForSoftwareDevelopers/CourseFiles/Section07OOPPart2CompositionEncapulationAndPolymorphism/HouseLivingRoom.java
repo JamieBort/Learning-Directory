e266fc669a68f8bf015807dcdea67a05b46e6b6c
Jamie Bort <jamiebort@gmail.com>
2021-05-13 10:01:07 -0400

    Created the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section07OOPPart2CompositionEncapulationAndPolymorphism/HouseLivingRoom.java file.

---
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section07OOPPart2CompositionEncapulationAndPolymorphism/HouseLivingRoom.java
@@ -0,0 +1,38 @@
+// Copied from CompositionPart2ChallengeExercise/ directory. Which has now since been deleted.
+
+
+// // Create a single room of a house using composition.
+// // Think about the things that should be included in the room.
+// // Maybe physical parts of the house but furniture as well
+// // Add at least one method to access an object via a getter and
+// // then that objects public method as you saw in the previous video
+// // then add at least one method to hide the object e.g. not using a getter
+// // but to access the object used in composition within the main class
+// // like you saw in this video.
+
+// package com.company;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+// 	// write your code here
+//     }
+// }
+
+
+// package com.company;
+
+// public class House {
+//     private LivingRoom livingRoom; //    a field. This is a has-a relationship.
+// //    constructor
+// //    methods
+// }
+
+// package com.company;
+
+// public class LivingRoom {
+// //    fields
+// //    constructor
+// //    methods
+// }
+
