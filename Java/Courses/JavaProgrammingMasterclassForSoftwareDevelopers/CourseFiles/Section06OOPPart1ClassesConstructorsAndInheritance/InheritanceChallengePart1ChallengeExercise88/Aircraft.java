68a637081fc307dc9ee1295367b35d387361dc9c
Jamie Bort <jamiebort@gmail.com>
2021-05-14 11:34:55 -0400

    Created a few more Java classes.

--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section06OOPPart1ClassesConstructorsAndInheritance/InheritanceChallengePart1ChallengeExercise88/Aircraft.java
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section06OOPPart1ClassesConstructorsAndInheritance/InheritanceChallengePart1ChallengeExercise88/Aircraft.java
@@ -1,29 +1,23 @@
+// Modify the below to work with a Aircraft rather than a car.
+
 // JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section06OOPPart1ClassesConstructorsAndInheritance/InheritanceChallengePart1ChallengeExercise88
 
 package Section06OOPPart1ClassesConstructorsAndInheritance.InheritanceChallengePart1ChallengeExercise88;
 
-public class Car extends Vehicle {
+public class Aircraft extends Vehicle {
 // have windows go up and down
 // default position is up
 // have headlights - headlights are on/off
 // which ones are not working
 // default headlights are on
 // has sound system - it's working or not?
-private int numStationaryWindows;
-private int numNonStationaryWindows;
-private boolean headLightOnStatus;
-private boolean headLightWorkingStatus;
-private boolean soundSystemWorkingStatus;
-private int numWheels;
-private int milageWheels;
 
-
-    public Car(int fuel) {
+    public Aircraft(int fuel) {
         super(fuel);
         //TODO Auto-generated constructor stub
     }
 
-    public Car(int fuel, int acceleration) { // is this redundant?
+    public Aircraft(int fuel, int acceleration) { // is this redundant?
         super(fuel, acceleration);
         //TODO Auto-generated constructor stub
     }
