7fdad9b7034abd537c75ad8dab4a061104b88e23
Jamie Bort <jamiebort@gmail.com>
2021-05-23 15:41:56 -0400

    Created the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section08ArraysJavaInbuiltListsAutoboxingAndUnboxing/LinkedListsPart1_116/ directory.

---
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section08ArraysJavaInbuiltListsAutoboxingAndUnboxing/LinkedListsPart1_116/Customer.java
@@ -0,0 +1,40 @@
+package Section08ArraysJavaInbuiltListsAutoboxingAndUnboxing.LinkedListsPart1_116;
+
+public class Customer {
+    private String name;
+    private double balance;
+    
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return double return the balance
+     */
+    public double getBalance() {
+        return balance;
+    }
+
+    /**
+     * @param balance the balance to set
+     */
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+}
