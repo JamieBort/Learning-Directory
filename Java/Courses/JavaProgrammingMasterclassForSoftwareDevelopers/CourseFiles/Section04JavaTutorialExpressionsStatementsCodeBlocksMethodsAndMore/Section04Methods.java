16001ed6842599c2a47353073c0cb2574536d579
Jamie Bort <jamiebort@gmail.com>
2021-05-25 12:39:46 -0400

    Modified the Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore/Section04Methods.java file.

--- Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore/Section04Methods.java
+++ Java/Courses/JavaProgrammingMasterclassForSoftwareDevelopers/CourseFiles/Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore/Section04Methods.java
@@ -1,22 +1,45 @@
 package Section04JavaTutorialExpressionsStatementsCodeBlocksMethodsAndMore;
 
 public class Section04Methods {
-
-    public void Seconds_and_Minutes_Challenge_60(int minutes, int seconds){
-        Seconds_and_Minutes_Challenge_60 Example01 = new Seconds_and_Minutes_Challenge_60();
-        Example01.getDurationString(minutes,seconds);
+    public Section04Methods() {
+        System.out.println("Inside constructor of Section04Methods class.");
     }
 
-    // public void Floating_Point_Precision_and_a_Challenge_30() {
-    //     System.out.println("Floating_Point_Precision_and_a_Challenge_30()");
-    //     Floating_Point_Precision_and_a_Challenge_30 Example01 = new Floating_Point_Precision_and_a_Challenge_30();
-    //     Example01.Floating_Point_Precision_and_a_Challenge();
-    // }
+    public void float_and_double_Primitive_Types_29() {
+        System.out.println("Inside float_and_double_Primitive_Types_29() method of Section04Methods class.");
+        // float_and_double_Primitive_Types_29 Example01 = new
+        // float_and_double_Primitive_Types_29();
+        // Example01.float_and_double_Primitive_Types();
+    }
 
-    // public void float_and_double_Primitive_Types_29(){
-    //     System.out.println("float_and_double_Primitive_Types_29()");
-    //     float_and_double_Primitive_Types_29 Example01 = new float_and_double_Primitive_Types_29();
-    //     Example01.float_and_double_Primitive_Types();
-    // }
+    public void Floating_Point_Precision_and_a_Challenge_30() {
+        System.out.println("Inside Floating_Point_Precision_and_a_Challenge_30() method of Section04Methods class.");
+        // Floating_Point_Precision_and_a_Challenge_30
+        // floating_Point_Precision_and_a_Challenge_30 = new
+        // Floating_Point_Precision_and_a_Challenge_30();
+        // floating_Point_Precision_and_a_Challenge_30.Floating_Point_Precision_and_a_Challenge();
+    }
+
+    public void Coding_Exercise_1_Speed_Converter() {
+        System.out.println("Inside Coding_Exercise_1_Speed_Converter() method of Section04Methods class.");
+        Coding_Exercise_1_Speed_Converter coding_Exercise_1_Speed_Converter = new Coding_Exercise_1_Speed_Converter();
+        // coding_Exercise_1_Speed_Converter.toMilesPerHour(1.606);
+        coding_Exercise_1_Speed_Converter.toMilesPerHour(5.0);
+    }
+
+    public void Seconds_and_Minutes_Challenge_60(int minutes, int seconds) {
+        Seconds_and_Minutes_Challenge_60 seconds_and_Minutes_Challenge_60 = new Seconds_and_Minutes_Challenge_60();
+        seconds_and_Minutes_Challenge_60.getDurationString(minutes, seconds);
+    }
+
+    public void Coding_Exercise_8_Area_Calculator() {
+        System.out.println("Inside Coding_Exercise_8_Area_Calculator() method of Section04Methods class.");
+        Coding_Exercise_8_Area_Calculator coding_Exercise_8_Area_Calculator = new Coding_Exercise_8_Area_Calculator();
+        coding_Exercise_8_Area_Calculator.area(5.0); // 78.53.975
+        // coding_Exercise_8_Area_Calculator.calculation(coding_Exercise_8_Area_Calculator.area(5.0));
+        coding_Exercise_8_Area_Calculator.area(-1.0); // -1
+        coding_Exercise_8_Area_Calculator.area(5.0,4.0); // 20
+        coding_Exercise_8_Area_Calculator.area(-1.0,4.0); // -1
+    }
 
 }
