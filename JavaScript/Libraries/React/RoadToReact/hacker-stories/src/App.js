7808984fe2ce60d7ae9e0b6873e6857dfdb6d7ea
Jamie Bort <jamiebort@gmail.com>
2021-11-18 17:02:01 -0500

    Work through the 'Controlled Components' section.

--- JavaScript/Libraries/React/RoadToReact/hacker-stories/src/App.js
+++ JavaScript/Libraries/React/RoadToReact/hacker-stories/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	console.log('App renders');
+	const stories = [
+		{
+			title: 'React',
+			url: 'https://reactjs.org/',
+			author: 'Jordan Walke',
+			num_comments: 3,
+			points: 4,
+			objectID: 0,
+		},
+		{
+			title: 'Redux',
+			url: 'https://redux.js.org/',
+			author: 'Dan Abramov, Andrew Clark',
+			num_comments: 2,
+			points: 5,
+			objectID: 1,
+		},
+	];
+
+	// const [ searchTerm, setSearchTerm ] = React.useState('');
+	const [ searchTerm, setSearchTerm ] = React.useState('Re');
+
+	const handleSearch = (event) => {
+		console.log('App: ', event.target.value);
+		setSearchTerm(event.target.value);
+	};
+
+	const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
+	return (
+		<div>
+			<h1>My Hacker Stories</h1>
+			<Search search={searchTerm} onSearch={handleSearch} />
+			<hr />
+			<List list={searchedStories} />
+		</div>
+	);
+};
+
+const Search = ({ search, onSearch }) => (
+	<div>
+		<label htmlFor="search">Search: </label>
+		<input id="search" type="text" value={search} onChange={onSearch} />
+	</div>
+);
+
+const List = ({ list }) => {
+	return <ul>{list.map((item) => <Item key={item.objectID} item={item} />)}</ul>;
+};
+
+const Item = ({ item }) => (
+	<li>
+		<span>
+			<a href={item.url}>{item.title}</a>
+		</span>
+		<span>{item.author}</span>
+		<span>{item.num_comments}</span>
+		<span>{item.points}</span>
+	</li>
+);
 
 export default App;
