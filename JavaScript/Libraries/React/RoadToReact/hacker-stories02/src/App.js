ade5f3766258eeefa338a0cdcf334e4f59031133
Jamie Bort <jamiebort@gmail.com>
2021-12-15 20:50:47 -0500

    Good stopping piont for JavaScript/Libraries/React/RoadToReact/hacker-stories02/src/App.js.

--- JavaScript/Libraries/React/RoadToReact/hacker-stories02/src/App.js
+++ JavaScript/Libraries/React/RoadToReact/hacker-stories02/src/App.js
@@ -1,5 +1,18 @@
 import * as React from 'react';
 
+const useSemiPersistentState = (key, initialState) => {
+	const [ value, setValue ] = React.useState(localStorage.getItem(key) || initialState);
+
+	React.useEffect(
+		() => {
+			localStorage.setItem(key, value);
+		},
+		[ value, key ],
+	);
+
+	return [ value, setValue ];
+};
+
 function App() {
 	const stories = [
 		{
@@ -20,51 +33,127 @@ function App() {
 		},
 	];
 
+	// const initialStories = [
+	// 	{
+	// 		title: 'React',
+	// 		url: 'https://reactjs.org/',
+	// 		author: 'Jordan Walke',
+	// 		num_comments: 3,
+	// 		points: 4,
+	// 		objectID: 0,
+	// 	},
+	// 	{
+	// 		title: 'Redux',
+	// 		url: 'https://redux.js.org/',
+	// 		author: 'Dan Abramov, Andrew Clark',
+	// 		num_comments: 2,
+	// 		points: 5,
+	// 		objectID: 1,
+	// 	},
+	// ];
+	const [ booleanValue, setBooleanValueValue ] = React.useState({ status01: true, status02: false });
+	// const [ stories, setStories ] = React.useState(initialStories);
+	// const handleRemoveStory = (item) => {
+	// 	const newStories = stories.filter((story) => item.objectID !== story.objectID);
+	// 	setStories(newStories);
+	// };
+	const [ searchTerm, setSearchTerm ] = useSemiPersistentState('search', 'Re');
+
 	const handleSearch = (event) => {
-		console.log(event.target.value);
+		setSearchTerm(event.target.value);
+	};
+
+	const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
+	const handleClick = () => {
+		booleanValue.status01
+			? setBooleanValueValue({ status01: false, status02: true })
+			: setBooleanValueValue({ status01: true, status02: false });
 	};
 
 	return (
 		<div>
 			<h1>My Hacker Stories</h1>
-
-			<Search onSearch={handleSearch} />
-
-			<hr />
-
-			<List list={stories} />
+			<button onClick={handleClick}>Button</button>
+			<InputWithLabel
+				id="search"
+				label="Search"
+				value={searchTerm}
+				isFocused={booleanValue.status01}
+				onInputChange={handleSearch}
+			>
+				<strong>Search:</strong>
+			</InputWithLabel>
+			<List list={searchedStories} />
+			{/* <List list={searchedStories} onRemoveItem={handleRemoveStory} /> */}
+			<InputWithLabel
+				id="search"
+				label="Search"
+				value={searchTerm}
+				isFocused={booleanValue.status02}
+				onInputChange={handleSearch}
+			>
+				<strong>second:</strong>
+			</InputWithLabel>
 		</div>
 	);
 }
 
-const Search = (props) => {
-	const [ searchTerm, setSearchTerm ] = React.useState('');
+const InputWithLabel = ({ id, label, value, type = 'type', onInputChange, isFocused, children }) => {
+	// A
+	const inputRef = React.useRef();
 
-	const handleChange = (event) => {
-		console.log(event.target.value);
-		setSearchTerm(event.target.value);
-		props.onSearch(event);
-	};
-
+	// C
+	React.useEffect(
+		() => {
+			if (isFocused && inputRef.current) {
+				// D
+				inputRef.current.focus();
+			}
+		},
+		[ isFocused ],
+	);
 	return (
-		<div>
-			<label htmlFor="search">Search: </label>
-			<input id="search" type="text" onChange={handleChange} />
-			<p>{searchTerm}</p>
-		</div>
+		<React.Fragment>
+			<label htmlFor={id}>{children}</label>
+			&nbsp;
+			{/* B */}
+			<input ref={inputRef} id={id} type={type} value={value} autoFocus={isFocused} onChange={onInputChange} />
+		</React.Fragment>
 	);
 };
 
-const List = (props) => <ul>{props.list.map((item) => <Item key={item.objectID} item={item} />)}</ul>;
+// I don't think this is used anymore.
+const Search = ({ search, onSearch }) => (
+	<React.Fragment>
+		<div>
+			<label htmlFor="search">Search: </label>
+			<input id="search" type="text" value={search} onChange={onSearch} />
+		</div>
+	</React.Fragment>
+);
 
-const Item = (props) => (
+const List = ({ list }) => <ul>{list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />)}</ul>;
+// const List = ({ list, onRemoveItem }) => (
+//   <ul>
+//     {list.map((item) => (
+//       <Item
+//         key={item.objectID}
+//         item={item}
+//         onRemoveItem={onRemoveItem}
+//       />
+//     ))}
+//   </ul>
+// );
+
+const Item = ({ title, url, author, num_comments, points }) => (
 	<li>
 		<span>
-			<a href={props.item.url}>{props.item.title}</a>
+			<a href={url}>{title}</a>
 		</span>
-		<span>{props.item.author}</span>
-		<span>{props.item.num_comments}</span>
-		<span>{props.item.points}</span>
+		<span>{author}</span>
+		<span>{num_comments}</span>
+		<span>{points}</span>
 	</li>
 );
 
