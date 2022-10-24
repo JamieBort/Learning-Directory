f218f896886dd8d86a05c7da43801a4723e4f30f
Jamie Bort <jamiebort@gmail.com>
2022-10-23 21:39:28 -0500

    Created a file and modified another.

--- JavaScript/CodingChallenges/ProjectEuler/003LargestPrimeFactorProjectEulerJavaScript/scratch.js
+++ JavaScript/CodingChallenges/ProjectEuler/003LargestPrimeFactorProjectEulerJavaScript/scratch.js
@@ -1,23 +1,15 @@
-let num = 13195;
-let list_of_prime_numbers = [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 ];
+const num = 13195;
+const list_of_prime_numbers = [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 ];
+let theList = [];
+let tempNum = num;
+let counter = 0;
 
-// finding the first integer
-list_of_prime_numbers.every((element) => {
-	// if (element == 7) return false;
-	if (Number.isInteger(num / element)) {
-		console.log('winner:', num / element);
-		return false;
-	}
+// while ((tempNum != 1)) {
+while (counter < 4) {
+	console.log(counter);
+	counter++;
+	console.log(counter);
+	console.log('====');
+}
 
-	console.log(element);
-	return true;
-});
-
-console.log(num / 2);
-console.log(num / 3);
-console.log(num / 5); // 5 and 2639
-
-console.log(2639 / 7);
-
-console.log(377 / 13);
-console.log(29 / 29);
+console.log(theList);
