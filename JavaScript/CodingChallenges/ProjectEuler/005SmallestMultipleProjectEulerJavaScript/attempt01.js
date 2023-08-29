accf1e93e88eb0d6801f5e435ee981ba36617066
Jamie Bort <jamiebort@gmail.com>
2023-08-29 10:01:32 -0400

    Renamed JavaScript/CodingChallenges/ProjectEuler/SmallestMultipleProjectEulerJavaScript/ to JavaScript/CodingChallenges/ProjectEuler/005SmallestMultipleProjectEulerJavaScript/.

--- JavaScript/CodingChallenges/ProjectEuler/005SmallestMultipleProjectEulerJavaScript/attempt01.js
+++ JavaScript/CodingChallenges/ProjectEuler/005SmallestMultipleProjectEulerJavaScript/attempt01.js
@@ -1,3 +1,7 @@
+// attempt01.js
+
+// Old title: SmallestMultiple.js
+
 // from: https://projecteuler.net/problem=5
 // 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
@@ -13,55 +17,53 @@
 
 var array = [];
 for (i = 1; i < 11; i++) {
-    if (9 % i != 0) {
-        // console.log(i);
-        array.push(i);
-    }
+  if (9 % i != 0) {
+    // console.log(i);
+    array.push(i);
+  }
 }
 console.log(array); // [2, 4, 5, 6, 7, 8, 10]
 
 var array2 = [];
 for (i = 1; i < 11; i++) {
-    if (8 % i != 0) {
-        // console.log(i);
-        array2.push(i);
-    }
+  if (8 % i != 0) {
+    // console.log(i);
+    array2.push(i);
+  }
 }
 console.log(array2); // [3, 5, 6, 7, 9, 10]
 
 var array3 = [];
 for (i = 1; i < 11; i++) {
-    if (7 % i != 0) {
-        // console.log(i);
-        array3.push(i);
-    }
+  if (7 % i != 0) {
+    // console.log(i);
+    array3.push(i);
+  }
 }
 console.log(array3); // [2, 3, 4, 5, 6, 8, 9, 10]
 
 var array4 = [];
 for (i = 1; i < 11; i++) {
-    array4.push(i);
+  array4.push(i);
 }
 console.log(array4);
 
 var array5 = [];
 array4.forEach(function (element) {
-    for (i = 1; i < 11; i++) {
+  for (i = 1; i < 11; i++) {
     // var g =element*2;
     // console.log(g);
 
     if (i != element) {
-    
-        if (i % element != 0) {
-            array5.push(element);
-            // return;
+      if (i % element != 0) {
+        array5.push(element);
+        // return;
         //     console.log(element + " does not divide evenly into " + i);
-        }
+      }
 
-        if (i % element === 0) {
-            console.log(element + " does divide evenly into " + i);
-            
-        }
+      if (i % element === 0) {
+        console.log(element + " does divide evenly into " + i);
+      }
     }
 
     // if (9 != element) {
@@ -72,40 +74,34 @@ array4.forEach(function (element) {
     //         console.log("This number does divide evenly into 9: " + element);
     //     }
     // }
-}
+  }
 });
 console.log(array4);
 console.log(array5);
 
 var array5 = [];
 array4.forEach(function (element) {
-    for (i = 1; i < 11; i++) {
-        if (i != element) {
-            if (8 % element != 0) {
-                console.log(element + " does not divide evenly into " + i);
-            }
-        }
-        if (i != element) {
-            if (9 % element != 0) {
-                console.log(element + " does not divide evenly into " + i);
-            }
-        }
+  for (i = 1; i < 11; i++) {
+    if (i != element) {
+      if (8 % element != 0) {
+        console.log(element + " does not divide evenly into " + i);
+      }
     }
+    if (i != element) {
+      if (9 % element != 0) {
+        console.log(element + " does not divide evenly into " + i);
+      }
+    }
+  }
 });
 console.log(array4);
 console.log(array5);
 
 var array5 = [];
 array4.forEach(function (element) {
-    
-        
-        if ((9 % element != 0) && (8 % element != 0) && (7 % element != 0)) {
-                // console.log(element + " does not divide evenly into " + 9);
-                console.log(element);
-            
-        
-    }
-
-    
+  if (9 % element != 0 && 8 % element != 0 && 7 % element != 0) {
+    // console.log(element + " does not divide evenly into " + 9);
+    console.log(element);
+  }
 });
 console.log(array5);
