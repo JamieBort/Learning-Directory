4fa7477154fa41aa350b300924b9e151b308042d
Jamie Bort <jamiebort@gmail.com>
2023-09-09 14:08:36 -0400

    Moved the to the JavaScript/CodingChallenges/TheIronYard/ directory JavaScript/TheIronYard/ directory.

--- JavaScript/CodingChallenges/CodeWars/AmISafeToDriveCodeWarsJavaScript/TimeConvertTwo.js
+++
@@ -1,7 +0,0 @@
-function timeStringToMinutesInteger(time) {
-  var firstVaribale = time.split(':');
-  const totalMinutes = parseInt(firstVaribale[0]) * 60 + parseInt(firstVaribale[1]);
-  console.log(typeof (totalMinutes));
-}
-
-timeStringToMinutesInteger("4:00");
